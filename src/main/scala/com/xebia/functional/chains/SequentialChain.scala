faa6e6af8c03bf1cb27cd755f2d081c1a9aa8e82
yago <Yawolf@users.noreply.github.com>
2023-04-18 15:24:22 +0200

    A little bit more safer

--- src/main/scala/com/xebia/functional/chains/SequentialChain.scala
+++ src/main/scala/com/xebia/functional/chains/SequentialChain.scala
@@ -3,16 +3,17 @@ package com.xebia.functional.chains
 import cats.MonadThrow
 import cats.implicits.*
 import cats.data.NonEmptySeq
+import eu.timepit.refined.types.string.NonEmptyString
 
 class SequentialChain[F[_]: MonadThrow] private (
     chains: NonEmptySeq[BaseChain[F]],
-    outputVariables: NonEmptySeq[String]
+    outputVariables: NonEmptySeq[NonEmptyString]
 ):
   def run(inputs: Map[String, String]): F[Map[String, String]] =
     chains
       .foldLeft(MonadThrow[F].pure(inputs))((fi, c) => fi.flatMap(i => c.run(i).map(_ ++ i)))
-      .map(_.filterKeys(outputVariables.contains_(_)).toMap)
+      .map(_.filterKeys(outputVariables.map(_.toString()).contains_(_)).toMap)
 
 object SequentialChain:
-  def make[F[_]: MonadThrow](chains: NonEmptySeq[BaseChain[F]], outputVariables: NonEmptySeq[String]): SequentialChain[F] =
+  def make[F[_]: MonadThrow](chains: NonEmptySeq[BaseChain[F]], outputVariables: NonEmptySeq[NonEmptyString]): SequentialChain[F] =
     SequentialChain[F](chains, outputVariables)
