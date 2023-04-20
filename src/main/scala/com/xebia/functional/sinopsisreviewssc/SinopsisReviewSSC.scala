764116c99d7e41d28e6b550bf3b9a2e5dff274b4
yago <Yawolf@users.noreply.github.com>
2023-04-20 10:04:14 +0200

    Recovering xor for "genInputs". SimpleSequentialChain Example added.

---
+++ src/main/scala/com/xebia/functional/sinopsisreviewssc/SinopsisReviewSSC.scala
@@ -0,0 +1,73 @@
+package com.xebia.functional.sinopsisreviewssc
+
+import cats.effect.{IO, IOApp, Resource}
+import com.xebia.functional.config.*
+
+import scala.concurrent.duration._
+import com.xebia.functional.chains.*
+import com.xebia.functional.embeddings.openai.models.*
+import com.xebia.functional.llm.openai.OpenAIClient
+import com.xebia.functional.vectorstores.postgres.*
+import eu.timepit.refined.types.string.NonEmptyString
+import cats.data.NonEmptySeq
+import com.xebia.functional.prompt.PromptTemplate
+
+object QASimpleSequentialChain extends IOApp.Simple:
+  override def run: IO[Unit] =
+    val OPENAI_TOKEN = "<place-your-openai-token-here>"
+    val openAIConfig = OpenAIConfig(OPENAI_TOKEN, 5.seconds, 5, 1000)
+    lazy val openAIClient = OpenAIClient[IO](openAIConfig)
+
+    val sinopsisTempl = """
+    You are a playwright. Given a title of play, it is your job to write a sinopsis for that title.
+
+    Title: {title}
+    Playwright: This is a synopsis for the above play:
+    """
+
+    val reviewTempl = """
+    You are a play critic from the New York Times. Given the sinopsis of play, it is you job to write a review for that play.
+
+    Play Sinopsis: {synopsis}
+    Review from a New York Times play critic of the above play:
+    """
+
+    val result = for
+      promptTemplateS <- Resource.eval(PromptTemplate.fromTemplate[IO](template = sinopsisTempl, inputVariables = List("title")))
+      promptTemplateR <- Resource.eval(PromptTemplate.fromTemplate[IO](template = reviewTempl, inputVariables = List("synopsis")))
+
+      synopsisChain = LLMChain.make(
+        llm = openAIClient,
+        promptTemplate = promptTemplateS,
+        llmModel = "davinci",
+        user = "testing",
+        echo = false,
+        n = 1,
+        temperature = 0.5,
+        onlyOutput = true
+      )
+
+      reviewChain = LLMChain.make(
+        llm = openAIClient,
+        promptTemplate = promptTemplateR,
+        llmModel = "davinci",
+        user = "testing",
+        echo = false,
+        n = 1,
+        temperature = 0.5,
+        onlyOutput = true
+      )
+
+      chains = NonEmptySeq(synopsisChain, Seq(reviewChain))
+
+      inputKey = NonEmptyString.from("input").toOption.get
+      outputKey = NonEmptyString.from("output").toOption.get
+
+      ssc <- SimpleSequentialChain.resource[IO](chains, inputKey, outputKey)
+      response <- Resource.eval(ssc.run("A beautiful day in the mountains ends up turning into a nightmare for a group of friends."))
+    yield (response)
+
+    result.use(response =>
+      println(response)
+      IO.unit
+    )
