a6c0d209affe2cc5797f260289f6afd104e7cbea
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-04-20 18:42:07 +0200

    Add OpenAI client (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,68 +4,64 @@ group = "com.xebia.functional"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 plugins {
     base
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.spotless)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = JavaVersion.VERSION_11.majorVersion
-        }
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
+  jvm {
+    compilations.all {
+      kotlinOptions.jvmTarget = JavaVersion.VERSION_11.majorVersion
     }
-    js(IR) {
-        browser {
-            commonWebpackConfig {
-                cssSupport {
-                    enabled.set(true)
-                }
-            }
+    withJava()
+    testRuns["test"].executionTask.configure {
+      useJUnitPlatform()
+    }
+  }
+  js(IR) {
+    browser {
+      commonWebpackConfig {
+        cssSupport {
+          enabled.set(true)
         }
+      }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+  }
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  when {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux" -> linuxX64("native")
+    isMingwX64 -> mingwX64("native")
+    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
 
-    
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(libs.arrow.core)
-                implementation(libs.open.ai)
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        api(libs.arrow.fx)
+        api(libs.bundles.ktor.client)
+        implementation(libs.kotlinx.serialization.json)
+      }
     }
+    commonTest {
+      dependencies {
+        implementation(kotlin("test"))
+      }
+    }
+  }
 }
 
 spotless {
