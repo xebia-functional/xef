12caaa1c29a9d62f0fe0708ed96fb5a3020a9621
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-04-21 09:33:26 +0200

    Remove Jinja until we support it. https://app.clickup.com/t/865c6434x

--- src/main/scala/com/xebia/functional/prompt/models/TemplateFormat.scala
+++ src/main/scala/com/xebia/functional/prompt/models/TemplateFormat.scala
@@ -1,5 +1,4 @@
 package com.xebia.functional.prompt.models
 
 enum TemplateFormat(val name: String):
-  case Jinja2 extends TemplateFormat("jinja2")
   case FString extends TemplateFormat("f-string")
