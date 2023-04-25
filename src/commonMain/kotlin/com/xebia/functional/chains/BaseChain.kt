9f13be5d888c1dc1502d0e83f8e15981986eef48
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-04-25 11:14:53 +0200

    feat: add first implementation of LLMChain

--- src/commonMain/kotlin/com/xebia/functional/chains/BaseChain.kt
+++ src/commonMain/kotlin/com/xebia/functional/chains/BaseChain.kt
@@ -1,10 +1,37 @@
 package com.xebia.functional.chains
 
+import arrow.core.raise.Raise
+
 interface BaseChain {
+
+    val config: Config
+
     suspend fun call(inputs: Map<String, String>): Map<String, String>
-    suspend fun run(inputs: String)
-    suspend fun run(inputs: Map<String, String>)
-    suspend fun prepareInputs(inputs: String)
-    suspend fun prepareInputs(inputs: Map<String, String>)
-    suspend fun prepareOutputs(inputs: Map<String, String>, outputs: Map<String, String>)
+
+    suspend fun Raise<InvalidChainInputs>.run(input: String): Map<String, String> {
+        val preparedInputs = prepareInputs(input)
+        val result = call(preparedInputs)
+        return prepareOutputs(preparedInputs, result)
+    }
+
+    suspend fun Raise<InvalidChainInputs>.run(inputs: Map<String, String>): Map<String, String> {
+        val preparedInputs = prepareInputs(inputs)
+        val result = call(preparedInputs)
+        return prepareOutputs(preparedInputs, result)
+    }
+
+    private suspend fun Raise<InvalidChainInputs>.prepareInputs(
+        inputs: String
+    ): Map<String, String> =
+        config.genInputsFromString(inputs).bind()
+
+    private suspend fun Raise<InvalidChainInputs>.prepareInputs(
+        inputs: Map<String, String>
+    ): Map<String, String> =
+        config.genInputs(inputs).bind()
+
+    private suspend fun prepareOutputs(
+        inputs: Map<String, String>, outputs: Map<String, String>
+    ): Map<String, String> =
+        if (config.onlyOutputs) outputs else inputs + outputs
 }
--- src/commonMain/kotlin/com/xebia/functional/chains/LLMChain.kt
+++ src/commonMain/kotlin/com/xebia/functional/chains/LLMChain.kt
@@ -1,2 +1,44 @@
 package com.xebia.functional.chains
 
+import com.xebia.functional.llm.openai.CompletionChoice
+import com.xebia.functional.llm.openai.CompletionRequest
+import com.xebia.functional.llm.openai.OpenAIClient
+import com.xebia.functional.prompt.PromptTemplate
+
+@Suppress("LongParameterList")
+suspend fun LLMChain(
+    llm: OpenAIClient,
+    promptTemplate: PromptTemplate,
+    llmModel: String,
+    user: String,
+    echo: Boolean,
+    n: Int,
+    temperature: Double,
+    onlyOutput: Boolean
+): BaseChain = object : BaseChain {
+
+    override val config = Config(promptTemplate.inputKeys.toSet(), setOf("answer"), onlyOutput)
+
+    override suspend fun call(inputs: Map<String, String>): Map<String, String> {
+        val prompt = preparePrompt(inputs)
+
+        val request = CompletionRequest(
+            model = llmModel,
+            user = user,
+            prompt = prompt,
+            echo = echo,
+            n = n,
+            temperature = temperature,
+        )
+
+        val completions = llm.createCompletion(request)
+        return formatOutput(completions)
+    }
+
+    private suspend fun preparePrompt(inputs: Map<String, String>): String = promptTemplate.format(inputs)
+
+    private fun formatOutput(completions: List<CompletionChoice>): Map<String, String> =
+        config.outputKeys.associateWith {
+            completions.joinToString(", ") { it.text }
+        }
+}
