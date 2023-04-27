dbcd6fdbe12afa0816e92c5b9a12825e65117b34
franciscodr <francisco.d@47deg.com>
2023-04-27 13:39:38 +0200

    Use base URL for ktor client

--- src/commonMain/kotlin/com/xebia/functional/ktor.kt
+++ src/commonMain/kotlin/com/xebia/functional/ktor.kt
@@ -1,22 +1,17 @@
 package com.xebia.functional
 
-import arrow.core.Either
-import arrow.core.nonFatalOrThrow
 import arrow.fx.coroutines.ResourceScope
-import arrow.resilience.Schedule
-import arrow.resilience.ScheduleStep
-import com.xebia.functional.env.RetryConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.header
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.Url
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
-import kotlin.time.Duration
 
 inline fun <reified A> HttpRequestBuilder.configure(token: String, request: A): Unit {
   header("Authorization", "Bearer $token")
@@ -24,9 +19,12 @@ inline fun <reified A> HttpRequestBuilder.configure(token: String, request: A): 
   setBody(request)
 }
 
-suspend fun ResourceScope.httpClient(engine: HttpClientEngine): HttpClient =
+suspend fun ResourceScope.httpClient(engine: HttpClientEngine, baseUrl: Url): HttpClient =
   install({
     HttpClient(engine) {
       install(ContentNegotiation) { json() }
+      defaultRequest {
+        url(baseUrl.toString())
+      }
     }
   }) { client, _ -> client.close() }
--- src/commonMain/kotlin/com/xebia/functional/llm/openai/OpenAIClient.kt
+++ src/commonMain/kotlin/com/xebia/functional/llm/openai/OpenAIClient.kt
@@ -9,6 +9,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.request.post
+import io.ktor.http.path
 
 interface OpenAIClient {
   suspend fun createCompletion(request: CompletionRequest): List<CompletionChoice>
@@ -18,25 +19,29 @@ interface OpenAIClient {
 suspend fun ResourceScope.KtorOpenAIClient(
   engine: HttpClientEngine,
   config: OpenAIConfig
-): OpenAIClient = KtorOpenAIClient(httpClient(engine), config)
+): OpenAIClient = KtorOpenAIClient(httpClient(engine, config.baseUrl), config)
 
 private class KtorOpenAIClient(
   private val httpClient: HttpClient,
   private val config: OpenAIConfig
 ) : OpenAIClient {
 
-  private val baseUrl = "https://api.openai.com/v1"
-
   override suspend fun createCompletion(request: CompletionRequest): List<CompletionChoice> {
     val response = config.retryConfig.schedule().retry {
-      httpClient.post("$baseUrl/completions") { configure(config.token, request) }
+      httpClient.post {
+        url { path("completions") }
+        configure(config.token, request)
+      }
     }
     return response.body()
   }
 
   override suspend fun createEmbeddings(request: EmbeddingRequest): EmbeddingResult {
     val response = config.retryConfig.schedule().retry {
-      httpClient.post("$baseUrl/embeddings") { configure(config.token, request) }
+      httpClient.post {
+        url { path("embeddings") }
+        configure(config.token, request)
+      }
     }
     return response.body()
   }
