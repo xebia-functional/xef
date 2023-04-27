9759fbc8c2513749845d35da446a521bad6070bc
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-04-27 15:07:27 +0200

    Clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,14 @@ plugins {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_17
-  targetCompatibility = JavaVersion.VERSION_17
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
 }
 
 kotlin {
   jvm {
     compilations.all {
-      kotlinOptions.jvmTarget = JavaVersion.VERSION_17.majorVersion
+      kotlinOptions.jvmTarget = JavaVersion.VERSION_11.majorVersion
     }
     withJava()
     testRuns["test"].executionTask.configure {
