22f747f3d1ae02f84b2ab270a5077bd2afb584a8
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-04-27 15:10:38 +0200

    Format queries

--- src/commonMain/kotlin/com/xebia/functional/vectorstores/postgres.kt
+++ src/commonMain/kotlin/com/xebia/functional/vectorstores/postgres.kt
@@ -8,55 +8,68 @@ enum class PGDistanceStrategy(val strategy: String) {
   Euclidean("<->"), InnerProduct("<#>"), CosineDistance("<=>")
 }
 
-val createCollections = """CREATE TABLE langchain4k_collections (
+val createCollections: String =
+  """CREATE TABLE langchain4k_collections (
        uuid TEXT PRIMARY KEY,
        name TEXT UNIQUE NOT NULL
      );""".trimIndent()
 
-val createEmbeddings = """CREATE TABLE langchain4k_embeddings (
+val createEmbeddings: String =
+  """CREATE TABLE langchain4k_embeddings (
        uuid TEXT PRIMARY KEY,
        collection_id TEXT REFERENCES langchain4k_collections(uuid),
        embedding BLOB,
        content TEXT
      );""".trimIndent()
 
-val addVectorExtension = "CREATE EXTENSION IF NOT EXISTS vector;"
+val addVectorExtension: String =
+  "CREATE EXTENSION IF NOT EXISTS vector;"
 
-val createCollectionsTable = """CREATE TABLE IF NOT EXISTS langchain4k_collections (
+val createCollectionsTable: String =
+  """CREATE TABLE IF NOT EXISTS langchain4k_collections (
        uuid TEXT PRIMARY KEY,
        name TEXT UNIQUE NOT NULL
      );""".trimIndent()
 
-fun createEmbeddingTable(vectorSize: Int) = """CREATE TABLE IF NOT EXISTS langchain4k_embeddings (
+fun createEmbeddingTable(vectorSize: Int): String =
+  """CREATE TABLE IF NOT EXISTS langchain4k_embeddings (
        uuid TEXT PRIMARY KEY,
        collection_id TEXT REFERENCES langchain4k_collections(uuid),
        embedding vector($vectorSize),
        content TEXT
      );""".trimIndent()
 
-val addNewCollection = """INSERT INTO langchain4k_collections(uuid, name)
+val addNewCollection: String =
+  """INSERT INTO langchain4k_collections(uuid, name)
      VALUES (?, ?)
      ON CONFLICT DO NOTHING;""".trimIndent()
 
-val deleteCollection = """DELETE FROM langchain4k_collections
+val deleteCollection: String =
+  """DELETE FROM langchain4k_collections
      WHERE uuid = ?;""".trimIndent()
 
-val getCollection = """SELECT * FROM langchain4k_collections
+val getCollection: String =
+  """SELECT * FROM langchain4k_collections
      WHERE name = ?;""".trimIndent()
 
-val getCollectionById = """SELECT * FROM langchain4k_collections
+val getCollectionById: String =
+  """SELECT * FROM langchain4k_collections
      WHERE uuid = ?;""".trimIndent()
 
-val addNewDocument = """INSERT INTO langchain4k_embeddings(uuid, collection_id, embedding, content)
+val addNewDocument: String =
+  """INSERT INTO langchain4k_embeddings(uuid, collection_id, embedding, content)
      VALUES (?, ?, ?, ?);""".trimIndent()
 
-val deleteCollectionDocs = """DELETE FROM langchain4k_embeddings
+val deleteCollectionDocs: String =
+  """DELETE FROM langchain4k_embeddings
      WHERE collection_id = ?;""".trimIndent()
 
-val addNewText = """INSERT INTO langchain4k_embeddings(uuid, collection_id, embedding, content)
+val addNewText: String =
+  """INSERT INTO langchain4k_embeddings(uuid, collection_id, embedding, content)
      VALUES (?, ?, ?::vector, ?);""".trimIndent()
 
-fun searchSimilarDocument(distance: PGDistanceStrategy) = """SELECT content FROM langchain4k_embeddings
+fun searchSimilarDocument(distance: PGDistanceStrategy): String =
+  """SELECT content FROM langchain4k_embeddings
      WHERE collection_id = ?
      ORDER BY embedding
      ${distance.strategy} ?::vector
