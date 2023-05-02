1e754a4d8e2e9cb6f5e4164298ef80040f640211
Juan Pedro Moreno <4879373+juanpedromoreno@users.noreply.github.com>
2023-05-02 22:09:46 +0200

    Adds a scala gradle module where.. (#19)
    will be placing the scala version of the library. Later on the amount of dependencies will be reduced according to the kotlin migration

---
+++ scala/build.gradle.kts
@@ -0,0 +1,34 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+plugins {
+    scala
+    alias(libs.plugins.scala.multiversion)
+}
+
+dependencies {
+    implementation(libs.ciris.core)
+    implementation(libs.ciris.refined)
+    implementation(libs.ciris.http4s)
+    implementation(libs.http4s.dsl)
+    implementation(libs.http4s.client)
+    implementation(libs.http4s.circe)
+    implementation(libs.http4s.emberClient)
+    implementation(libs.doobie.core)
+    implementation(libs.doobie.postgres)
+    implementation(libs.doobie.hikari)
+    implementation(libs.doobie.munit)
+    implementation(libs.circe)
+    implementation(libs.cats.effect)
+    implementation(libs.logger)
+    implementation(libs.openai)
+    implementation(libs.cats.retry)
+    implementation(libs.memeid)
+    testImplementation(libs.munit.core)
+    testImplementation(libs.munit.cats.effect)
+    testImplementation(libs.scala.testcontainers.munit)
+    testImplementation(libs.scala.testcontainers.postgresql)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnit()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,7 @@ pluginManagement {
 rootProject.name = "langchain4k"
 include("nodejs-commandexecutor")
 include("example")
+
+
+include("langchain4k-scala")
+project(":langchain4k-scala").projectDir = file("scala")
