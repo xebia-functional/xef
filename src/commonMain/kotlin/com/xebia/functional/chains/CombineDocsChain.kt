484c2c3be9b4e73a251f8ed8b87a9292095e4fab
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-05-02 22:52:42 +0200

    test: create chain test utils

--- src/commonMain/kotlin/com/xebia/functional/chains/CombineDocsChain.kt
+++ src/commonMain/kotlin/com/xebia/functional/chains/CombineDocsChain.kt
@@ -7,39 +7,39 @@ import com.xebia.functional.chains.LLMChain
 import com.xebia.functional.llm.openai.OpenAIClient
 import com.xebia.functional.prompt.PromptTemplate
 
-interface CombineDocumentsChain : Chain {
-    suspend fun combineDocs(documents: List<Document>): Map<String, String>
+interface CombineDocsChain : Chain {
+    suspend fun combine(documents: List<Document>): Map<String, String>
 }
 
 @Suppress("LongParameterList")
-suspend fun CombineDocumentsChain(
+suspend fun CombineDocsChain(
     llm: OpenAIClient,
     promptTemplate: PromptTemplate,
     documents: List<Document>,
     documentVariableName: String,
     outputVariable: String,
     chainOutput: Chain.ChainOutput = Chain.ChainOutput.OnlyOutput
-): CombineDocumentsChain = object : CombineDocumentsChain {
+): CombineDocsChain = object : CombineDocsChain {
 
     private val inputKeys = promptTemplate.inputKeys.toSet() - setOf(documentVariableName)
     private val outputKeys = setOf("answer")
 
     override val config: Chain.Config = Chain.Config(inputKeys, outputKeys, chainOutput)
 
-    override suspend fun combineDocs(documents: List<Document>): Map<String, String> {
+    override suspend fun combine(documents: List<Document>): Map<String, String> {
         val mergedDocs = documents.joinToString("\n") { it.content }
         return mapOf(documentVariableName to mergedDocs)
     }
 
     override suspend fun call(inputs: Map<String, String>): Either<Chain.Error, Map<String, String>> {
         val llmChain = LLMChain(
-            llm = llm,
-            promptTemplate = promptTemplate,
+            llm,
+            promptTemplate, 
             outputVariable = outputVariable,
-            chainOutput = chainOutput)
+            chainOutput = chainOutput
+        )
 
-        val documentInput = combineDocs(documents)
-        val totalInputs = documentInput + inputs
+        val totalInputs = combine(documents) + inputs
         return llmChain.run(totalInputs)
     }
 }
--- src/commonTest/kotlin/com/xebia/functional/chains/CombineDocsChainSpec.kt
+++ src/commonTest/kotlin/com/xebia/functional/chains/CombineDocsChainSpec.kt
@@ -1,22 +1,17 @@
 package com.xebia.functional.chains
 
 import arrow.core.raise.either
-import com.xebia.functional.llm.openai.CompletionChoice
-import com.xebia.functional.llm.openai.CompletionRequest
-import com.xebia.functional.llm.openai.EmbeddingRequest
-import com.xebia.functional.llm.openai.EmbeddingResult
-import com.xebia.functional.llm.openai.OpenAIClient
 import com.xebia.functional.prompt.PromptTemplate
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.spec.style.StringSpec
 
-class LLMChainSpec : StringSpec({
+class CombineDocsChainSpec : StringSpec({
     "LLMChain should return a prediction with just the output" {
         val template = "Tell me {foo}."
         either {
             val promptTemplate = PromptTemplate(template, listOf("foo"))
-            val chain = LLMChain(testLLM, promptTemplate, outputVariable = "answer")
+            val chain = LLMChain(llm, promptTemplate, outputVariable = "answer")
             chain.run("a joke").bind()
         } shouldBeRight mapOf("answer" to "I'm not good at jokes")
     }
@@ -25,8 +20,7 @@ class LLMChainSpec : StringSpec({
         val template = "Tell me {foo}."
         either {
             val prompt = PromptTemplate(template, listOf("foo"))
-            val chain = LLMChain(testLLM, prompt, outputVariable = "answer",
-                chainOutput = Chain.ChainOutput.InputAndOutput)
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run("a joke").bind()
         } shouldBeRight mapOf("foo" to "a joke", "answer" to "I'm not good at jokes")
     }
@@ -35,8 +29,7 @@ class LLMChainSpec : StringSpec({
         val template = "My name is {name} and I'm {age} years old"
         either {
             val prompt = PromptTemplate(template, listOf("name", "age"))
-            val chain = LLMChain(testLLM, prompt, outputVariable = "answer",
-                chainOutput = Chain.ChainOutput.InputAndOutput)
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run(mapOf("age" to "28", "name" to "foo")).bind()
         } shouldBeRight mapOf("age" to "28", "name" to "foo", "answer" to "Hello there! Nice to meet you foo")
     }
@@ -45,8 +38,7 @@ class LLMChainSpec : StringSpec({
         val template = "My name is {name} and I'm {age} years old"
         either {
             val prompt = PromptTemplate(template, listOf("name", "age"))
-            val chain = LLMChain(testLLM, prompt, outputVariable = "answer",
-                chainOutput = Chain.ChainOutput.InputAndOutput)
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run(mapOf("age" to "28", "brand" to "foo")).bind()
         } shouldBeLeft
                 Chain.InvalidInputs(
@@ -57,25 +49,8 @@ class LLMChainSpec : StringSpec({
         val template = "My name is {name} and I'm {age} years old"
         either {
             val prompt = PromptTemplate(template, listOf("name", "age"))
-            val chain = LLMChain(testLLM, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run("foo").bind()
         } shouldBeLeft Chain.InvalidInputs("The expected inputs are more than one: {name}, {age}")
     }
 })
-
-val llm = object : OpenAIClient {
-    override suspend fun createCompletion(request: CompletionRequest): List<CompletionChoice> =
-        when(request.prompt) {
-            "Tell me a joke." ->
-                listOf(CompletionChoice("I'm not good at jokes", 1, "foo"))
-            "My name is foo and I'm 28 years old" ->
-                listOf(CompletionChoice("Hello there! Nice to meet you foo", 1, "foo"))
-            else -> listOf(CompletionChoice("foo", 1, "bar"))
-        }
-
-    override suspend fun createChatCompletion(request: ChatCompletionRequest): ChatCompletionResponse {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun createEmbeddings(request: EmbeddingRequest): EmbeddingResult = TODO()
-}
