9e23ed84a6981890205931c7d37d62ced8677445
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-05-02 22:17:18 +0200

    test: remove LLMChain max line length warnings

--- src/commonTest/kotlin/com/xebia/functional/chains/LLMChainSpec.kt
+++ src/commonTest/kotlin/com/xebia/functional/chains/LLMChainSpec.kt
@@ -20,8 +20,8 @@ class LLMChainSpec : StringSpec({
     "LLMChain should return a prediction with both output and inputs" {
         val template = "Tell me {foo}."
         either {
-            val promptTemplate = PromptTemplate(template, listOf("foo"))
-            val chain = LLMChain(llm, promptTemplate, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
+            val prompt = PromptTemplate(template, listOf("foo"))
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run("a joke").bind()
         } shouldBeRight mapOf("foo" to "a joke", "answer" to "I'm not good at jokes")
     }
@@ -29,8 +29,8 @@ class LLMChainSpec : StringSpec({
     "LLMChain should return a prediction with a more complex template" {
         val template = "My name is {name} and I'm {age} years old"
         either {
-            val promptTemplate = PromptTemplate(template, listOf("name", "age"))
-            val chain = LLMChain(llm, promptTemplate, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
+            val prompt = PromptTemplate(template, listOf("name", "age"))
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run(mapOf("age" to "28", "name" to "foo")).bind()
         } shouldBeRight mapOf("age" to "28", "name" to "foo", "answer" to "Hello there! Nice to meet you foo")
     }
@@ -38,17 +38,19 @@ class LLMChainSpec : StringSpec({
     "LLMChain should fail when inputs are not the expected ones from the PromptTemplate" {
         val template = "My name is {name} and I'm {age} years old"
         either {
-            val promptTemplate = PromptTemplate(template, listOf("name", "age"))
-            val chain = LLMChain(llm = llm, promptTemplate, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
+            val prompt = PromptTemplate(template, listOf("name", "age"))
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run(mapOf("age" to "28", "brand" to "foo")).bind()
-        } shouldBeLeft Chain.InvalidInputs("The provided inputs: {age}, {brand} do not match with chain's inputs: {name}, {age}")
+        } shouldBeLeft
+                Chain.InvalidInputs(
+                    "The provided inputs: {age}, {brand} do not match with chain's inputs: {name}, {age}")
     }
 
     "LLMChain should fail when using just one input but expecting more" {
         val template = "My name is {name} and I'm {age} years old"
         either {
-            val promptTemplate = PromptTemplate(template, listOf("name", "age"))
-            val chain = LLMChain(llm = llm, promptTemplate, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
+            val prompt = PromptTemplate(template, listOf("name", "age"))
+            val chain = LLMChain(llm, prompt, outputVariable = "answer", chainOutput = Chain.ChainOutput.InputAndOutput)
             chain.run("foo").bind()
         } shouldBeLeft Chain.InvalidInputs("The expected inputs are more than one: {name}, {age}")
     }
