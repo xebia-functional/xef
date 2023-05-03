e4e18476d7f4b411cf4041af7f4e0b271fbc3635
Francisco Diaz <francisco.d@47deg.com>
2023-05-03 17:43:59 +0200

    Create a subproject for Kotlin code (#27)
    * Create a subproject for Kotlin code
    
    * Update some project dependencies

--- scala/build.gradle.kts
+++ scala/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":langchain4k-kotlin"))
     implementation(libs.ciris.core)
     implementation(libs.ciris.refined)
     implementation(libs.ciris.http4s)
