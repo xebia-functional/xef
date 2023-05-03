3fdeac936a1d2a0e31bea622b09a4a2fed7df7ab
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-05-03 16:35:45 +0200

    feat: adjust CompletionChoice models and add example demo

--- example/src/main/kotlin/com/xebia/functional/langchain4k/chain/Clothes.kt
+++ example/src/main/kotlin/com/xebia/functional/langchain4k/chain/Clothes.kt
@@ -1,47 +1,71 @@
 package com.xebia.functional.langchain4k.chain
 
+import arrow.core.getOrElse
 import arrow.core.raise.either
+import arrow.core.raise.recover
 import arrow.fx.coroutines.resourceScope
+import com.xebia.functional.Document
 import com.xebia.functional.chains.VectorQAChain
 import com.xebia.functional.embeddings.OpenAIEmbeddings
 import com.xebia.functional.env.OpenAIConfig
 import com.xebia.functional.llm.openai.KtorOpenAIClient
+import com.xebia.functional.llm.openai.OpenAIClient
+import com.xebia.functional.loaders.BaseLoader
 import com.xebia.functional.loaders.TextLoader
 import com.xebia.functional.vectorstores.LocalVectorStore
 import io.github.oshai.KotlinLogging
+import okio.Path
 import okio.Path.Companion.toPath
+import java.io.File
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.time.ExperimentalTime
 
 @PublishedApi
 internal val logger = KotlinLogging.logger("Clothes")
 
+data class Error(val reason: String)
+
 @OptIn(ExperimentalTime::class)
 suspend fun main() {
     resourceScope {
         either {
-            val OPENAI_TOKEN = "<place-your-openai-token-here>"
+            val openAIConfig = recover({
+                val token = "<OPENAI_TOKEN>"
+                OpenAIConfig(token)
+            }) { raise(Error(it.joinToString(", "))) }
 
-            val openAIConfig = OpenAIConfig()
-            val openAiClient = KtorOpenAIClient(openAIConfig)
+            val openAiClient: OpenAIClient = KtorOpenAIClient(openAIConfig)
             val embeddings = OpenAIEmbeddings(openAIConfig, openAiClient, logger)
             val vectorStore = LocalVectorStore(embeddings)
 
-            val path = "/weather.txt".toPath()
-            val textLoader = TextLoader(path)
-            val docs = textLoader.load()
+            val resource: URL = javaClass.getResource("/docs/weather.txt") ?: raise(Error("Resource not found"))
+            val path: Path = File(resource.file).path.toPath()
+
+            val textLoader: BaseLoader = TextLoader(path)
+            val docs: List<Document> = textLoader.load()
             vectorStore.addDocuments(docs)
 
+            val numOfDocs = 10
             val outputVariable = "answer"
             val chain = VectorQAChain(
                 openAiClient,
                 vectorStore,
-                1,
+                numOfDocs,
                 outputVariable
             )
 
-            chain.run("")
-        }
+            val sdf = SimpleDateFormat("dd/MM/yyyy")
+            val currentDate: String = sdf.format(Date()) ?: raise(Error("Invalid date"))
+
+            val question = "If today is $currentDate, what clothes do you recommend should I wear this week?"
+            val response: Map<String, String> = recover({
+                chain.run(question).bind()
+            }) { raise(Error(it.reason)) }
+
+            println(response)
+
+        }.getOrElse { throw IllegalStateException(it.reason) }
     }
-
-
 }
