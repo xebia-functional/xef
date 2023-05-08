2d369c386126f07718aa2aaa863b89d6ba682ea7
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-05-08 20:51:11 +0200

    feat: better string formatting and additional test

--- kotlin/src/commonMain/kotlin/com/xebia/functional/prompt/ChatPromptTemplate.kt
+++ kotlin/src/commonMain/kotlin/com/xebia/functional/prompt/ChatPromptTemplate.kt
@@ -6,13 +6,6 @@ import arrow.core.raise.either
 
 data class InvalidInputs(val reason: String)
 
-fun Raise<InvalidInputs>.ChatPromptTemplate(
-    promptMessages: List<PromptTemplate<out Message>>
-): ChatPromptTemplate {
-    val inputKeys: List<String> = promptMessages.flatMap { it.inputKeys }
-    return ChatPromptTemplate(inputKeys, promptMessages)
-}
-
 interface ChatPromptTemplate {
     val promptMessages: List<PromptTemplate<out Message>>
     val inputKeys: List<String>
@@ -20,46 +13,56 @@ interface ChatPromptTemplate {
     suspend fun format(inputs: Map<String, String>): Either<InvalidInputs, String>
 
     suspend fun formatMessages(inputs: Map<String, String>): Either<InvalidInputs, List<Message>>
-
-    companion object {
-
-        operator fun invoke(
-            inputKeys: List<String>,
-            promptMessages: List<PromptTemplate<out Message>>
-        ): ChatPromptTemplate = object : ChatPromptTemplate {
-            override val promptMessages: List<PromptTemplate<out Message>> = promptMessages
-            override val inputKeys: List<String> = inputKeys
-
-            override suspend fun format(inputs: Map<String, String>): Either<InvalidInputs, String> =
-                either {
-                    val messages: List<Message> = formatMessages(inputs).bind()
-                    messages.joinToString(separator = "\n") { it.content }
-                }
-
-            override suspend fun formatMessages(
-                inputs: Map<String, String>
-            ): Either<InvalidInputs, List<Message>> =
-                either {
-                    val allInputs: Map<String, String> =
-                        PromptTemplate.mergePartialAndUserVariables(inputs, inputKeys)
-
-                    promptMessages.map { prompt ->
-                        val inputValues: Map<String, String> = createInputs(allInputs, prompt.inputKeys)
-                        prompt.format(inputValues)
-                    }
-                }
-
-            private fun Raise<InvalidInputs>.createInputs(
-                inputs: Map<String, String>, inputKeys: List<String>
-            ): Map<String, String> =
-                inputKeys.associateWith { inputKey ->
-                    inputs[inputKey] ?: raise(
-                        InvalidInputs("The provided inputs: " +
-                                inputs.keys.joinToString(", ") { "{$it}" } +
-                                " do not match with prompt's inputs: " +
-                                inputKeys.joinToString(", ") { "{$it}" })
-                    )
-                }
-        }
-    }
 }
+
+suspend fun ChatPromptTemplate(
+    promptMessages: List<PromptTemplate<out Message>>
+): ChatPromptTemplate = object : ChatPromptTemplate {
+    override val promptMessages: List<PromptTemplate<out Message>> = promptMessages
+    override val inputKeys: List<String> = promptMessages.flatMap { it.inputKeys }
+
+    override suspend fun format(inputs: Map<String, String>): Either<InvalidInputs, String> =
+        either {
+            val messages: List<Message> = formatMessages(inputs).bind()
+            messages.joinToString(separator = "\n") { message ->
+                when (message) {
+                    is HumanMessage -> message.format()
+                    is AIMessage -> message.format()
+                    is SystemMessage -> message.format()
+                    is ChatMessage -> message.format()
+                }
+            }
+        }
+
+    override suspend fun formatMessages(inputs: Map<String, String>): Either<InvalidInputs, List<Message>> =
+        either {
+            val allInputs: Map<String, String> =
+                PromptTemplate.mergePartialAndUserVariables(inputs, inputKeys)
+
+            promptMessages.map { prompt ->
+                val inputValues: Map<String, String> = createInputs(allInputs, prompt.inputKeys)
+                prompt.format(inputValues)
+            }
+        }
+
+    private fun Raise<InvalidInputs>.createInputs(
+        inputs: Map<String, String>, inputKeys: List<String>
+    ): Map<String, String> =
+        inputKeys.associateWith { inputKey ->
+            inputs[inputKey] ?: raise(
+                InvalidInputs("The provided inputs: " +
+                        inputs.keys.joinToString(", ") { "{$it}" } +
+                        " do not match with prompt's inputs: " +
+                        inputKeys.joinToString(", ") { "{$it}" })
+            )
+        }
+}
+
+private fun Message.format(): String =
+    "${type().capitalizedName()}: $content"
+
+private fun ChatMessage.format(): String =
+    "${this.role}: $content"
+
+private fun Type.capitalizedName() =
+    name.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
--- kotlin/src/commonTest/kotlin/com/xebia/functional/prompt/ChatPromptTemplateSpec.kt
+++ kotlin/src/commonTest/kotlin/com/xebia/functional/prompt/ChatPromptTemplateSpec.kt
@@ -5,7 +5,7 @@ import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.spec.style.StringSpec
 
 class ChatPromptTemplateSpec : StringSpec({
-    "ChatPromptTemplate format should return the formatted content of the messages" {
+    "ChatPromptTemplate format should return the correct formatted type and content of the messages" {
         either {
             val systemPrompt: PromptTemplate<SystemMessage> = PromptTemplate.system(
                 PromptTemplate(
@@ -30,11 +30,60 @@ class ChatPromptTemplateSpec : StringSpec({
                     "input_language" to "English",
                     "output_language" to "Spanish",
                     "text" to "What is your name?"
-                    )
+                )
             ).bind()
 
-        } shouldBeRight "You are a helpful assistant that translates English to Spanish.\n" +
-                "What is your name?"
+        } shouldBeRight
+                "System: You are a helpful assistant that translates English to Spanish.\n" +
+                "Human: What is your name?"
+    }
+
+    "ChatPromptTemplate format with chat messages should return the correct type, role and content of the messages" {
+        either {
+            val systemPrompt: PromptTemplate<SystemMessage> = PromptTemplate.system(
+                PromptTemplate(
+                    "{context}", listOf("context")
+                )
+            )
+
+            val humanPrompt: PromptTemplate<HumanMessage> = PromptTemplate.human(
+                PromptTemplate(
+                    "{question}", listOf("question")
+                )
+            )
+
+            val anakinPrompt: PromptTemplate<ChatMessage> = PromptTemplate.chat(
+                PromptTemplate(
+                    "{firstPhrase}",
+                    listOf("firstPhrase")
+                ), "Anakin"
+            )
+
+            val obiWanPrompt: PromptTemplate<ChatMessage> = PromptTemplate.chat(
+                PromptTemplate(
+                    "{secondPhrase}",
+                    listOf("secondPhrase")
+                ), "Obi-Wan"
+            )
+
+            val chatPrompt = ChatPromptTemplate(
+                listOf(systemPrompt, humanPrompt, anakinPrompt, obiWanPrompt)
+            )
+
+            chatPrompt.format(
+                mapOf(
+                    "firstPhrase" to "YOU TURNED HER AGAINST ME!?",
+                    "secondPhrase" to "You have done that yourself.",
+                    "question" to "What movie is this conversation from?",
+                    "context" to "You are a helpful assistant that helps on guessing movies."
+                )
+            ).bind()
+
+        } shouldBeRight
+                "System: You are a helpful assistant that helps on guessing movies.\n" +
+                "Human: What movie is this conversation from?\n" +
+                "Anakin: YOU TURNED HER AGAINST ME!?\n" +
+                "Obi-Wan: You have done that yourself."
     }
 
     "ChatPromptTemplate formatMessages should return the formatted list of messages" {
