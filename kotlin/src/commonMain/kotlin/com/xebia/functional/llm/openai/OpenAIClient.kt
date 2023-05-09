34ee429166930562900cafb7697a9832fea6ec5f
Juan Pedro Moreno <juanpedro.m@47deg.com>
2023-05-09 09:35:27 +0200

    No star imports

--- kotlin/src/commonMain/kotlin/com/xebia/functional/llm/openai/OpenAIClient.kt
+++ kotlin/src/commonMain/kotlin/com/xebia/functional/llm/openai/OpenAIClient.kt
@@ -9,7 +9,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.request.post
-import io.ktor.client.statement.*
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.path
 
 interface OpenAIClient {
