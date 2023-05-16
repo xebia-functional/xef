37ea59e688ca48bd2d52f8f8153fa55435e1e76d
yago <Yawolf@users.noreply.github.com>
2023-05-16 10:17:44 +0200

    Updating

--- scala/src/main/scala/com/xebia/functional/auto/AI.scala
+++ scala/src/main/scala/com/xebia/functional/auto/AI.scala
@@ -3,40 +3,47 @@ package com.xebia.functional.scala.auto
 import cats.effect.kernel.Async
 import cats.implicits.*
 import com.xebia.functional.scala.kotlin.CoroutineToIO
-import com.xebia.functional.auto.SerializationConfig as KtSerializationConfig
-import com.xebia.functional.auto.AIScope as KtAIScope
-import com.xebia.functional.tools.Agent as KtAgent
-import com.xebia.functional.llm.openai.LLMModel as KtLLMModel
-import com.xebia.functional.auto.AIKt
+import com.xebia.functional.xef.prompt.PromptTemplate as KtPromptTemplate
+import com.xebia.functional.xef.auto.SerializationConfig as KtSerializationConfig
+import com.xebia.functional.xef.auto.AIScope as KtAIScope
+import com.xebia.functional.xef.agents.ParameterlessAgent
+import com.xebia.functional.xef.llm.openai.LLMModel as KtLLMModel
+import com.xebia.functional.xef.auto.AIKt
 import unwrapped.*
 
-object AI:
+import scala.jdk.CollectionConverters.*
 
-  def adapter[F[_]: Async, A](block: AIScope[F] ?=> A): F[A] =
-    CoroutineToIO[F].runCancelable[KtAIScope] { (_, cont) =>
-      val ktAIScope: KtAIScope = ???
-      ktAIScope
-    }.map(AIScope.fromCore[F])
+object AI:
+  private type ContextualAgent = ParameterlessAgent[java.util.List[String]]
+
+  private def adapter[F[_]: Async, A](block: AIScope[F] ?=> A): F[A] =
+    CoroutineToIO[F]
+      .runCancelable[KtAIScope] { (_, cont) =>
+        val ktAIScope: KtAIScope = ???
+        ktAIScope
+      }.map(AIScope.fromCore[F])
       .map(block(using _))
 
-  def ai[F[_] : Async, A](block: AIScope[F] ?=> A): F[A] =
+  def ai[F[_]: Async, A](block: AIScope[F] ?=> A): F[A] =
     val f = adapter(block)
     CoroutineToIO[F].runCancelable[A] { (_, cont) =>
       AIKt.ai(???)
     }
 
-  final case class AIScope[F[_] : Async] private(kt: KtAIScope):
-    def agent[A](agent: KtAgent, scope: AIScope[F] ?=> A): F[A] = ???
+  final case class AIScope[F[_]: Async] private (kt: KtAIScope):
+    def context[A](agent: ContextualAgent, scope: AIScope[F] ?=> A): F[A] = ???
 
-    def agent[A](agents: List[KtAgent], scope: AIScope[F] ?=> A): F[A] = ???
+    def context[A](agents: List[ContextualAgent], scope: AIScope[F] ?=> A): F[A] = ???
 
-    def prompt[A](
-      prompt: String,
-      serializationConfig: KtSerializationConfig[A],
-      maxAttempts: Int = 5,
-      llmMode: KtLLMModel = KtLLMModel("gpt-3.5-turbo", KtLLMModel.Kind.Chat)
-    ): F[A] =
-      CoroutineToIO[F].runCancelable[A] { (_, cont) => kt.prompt(prompt, serializationConfig, maxAttempts, llmMode, cont) }
+    def promptMessage[A](question: String, model: KtLLMModel): F[List[String]] =
+      CoroutineToIO[F]
+        .runCancelable[java.util.List[String]]((_, cont) => kt.promptMessage(question, model, cont))
+        .map(_.asScala.toList)
+
+    def promptMessage[A](prompt: KtPromptTemplate[String], variables: Map[String, String], model: KtLLMModel): F[List[String]] =
+      CoroutineToIO[F]
+        .runCancelable[java.util.List[String]]((_, cont) => kt.promptMessage(prompt, variables.asJava, model, cont))
+        .map(_.asScala.toList)
 
   private object AIScope:
     def fromCore[F[_]: Async](coreAIScope: KtAIScope): AIScope[F] = new AIScope[F](coreAIScope)
