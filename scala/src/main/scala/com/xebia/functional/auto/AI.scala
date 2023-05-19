cb29b837a872fcaf9fd298a877cefcc57a5d483d
Alejandro Serrano <trupill@gmail.com>
2023-05-19 15:40:39 +0200

    Fix Scala

--- scala/src/main/scala/com/xebia/functional/auto/AI.scala
+++ scala/src/main/scala/com/xebia/functional/auto/AI.scala
@@ -5,7 +5,6 @@ import com.xebia.functional.xef.auto.AIScope as KtAIScope
 import com.xebia.functional.xef.auto.AIException
 import com.xebia.functional.xef.auto.AIKt
 import com.xebia.functional.xef.AIError
-import com.xebia.functional.xef.agents.Agent as KtAgent
 import com.xebia.functional.xef.llm.openai.LLMModel
 
 //def example(using AIScope): String =
@@ -31,9 +30,6 @@ object AI:
 end AI
 
 final case class AIScope(kt: KtAIScope):
-  def agent[A](agent: KtAgent[List[String]], scope: AIScope ?=> A): A = ???
-
-  def agent[A](agents: List[KtAgent[List[String]]], scope: AIScope ?=> A): A = ???
 
   // TODO: Design signature for Scala3 w/ Json parser (with support for generating Json Schema)?
   def prompt[A](
@@ -41,6 +37,12 @@ final case class AIScope(kt: KtAIScope):
       maxAttempts: Int = 5,
       llmMode: LLMModel = LLMModel.getGPT_3_5_TURBO
   ): A = ???
+
+  def promptMessage(
+      prompt: String,
+      maxAttempts: Int = 5,
+      llmMode: LLMModel = LLMModel.getGPT_3_5_TURBO
+  ): String = ???
 
 private object AIScope:
   def fromCore(coreAIScope: KtAIScope): AIScope = new AIScope(coreAIScope)
