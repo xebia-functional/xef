6405f84a62b40d322d2185019191eb6a3713940d
Raúl Raja Martínez <raulraja@gmail.com>
2023-05-19 19:00:33 +0200

    Remote PDF Loading (#75)
    * Remote PDF Loading
    
    * Remove unused import
    
    * Update integrations/pdf/src/main/kotlin/com/xebia/functional/xef/pdf/PDFLoader.kt
    
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>
    
    * Fix merge from main
    
    ---------
    
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>

--- integrations/pdf/src/main/kotlin/com/xebia/functional/xef/pdf/PDFLoader.kt
+++ integrations/pdf/src/main/kotlin/com/xebia/functional/xef/pdf/PDFLoader.kt
@@ -4,9 +4,28 @@ import com.xebia.functional.tokenizer.ModelType
 import com.xebia.functional.xef.loaders.BaseLoader
 import com.xebia.functional.xef.textsplitters.BaseTextSplitter
 import com.xebia.functional.xef.textsplitters.TokenTextSplitter
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.util.cio.*
+import io.ktor.utils.io.*
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
 import java.io.File
+
+suspend fun pdf(
+  url: String,
+  splitter: BaseTextSplitter = TokenTextSplitter(modelType = ModelType.GPT_3_5_TURBO, chunkSize = 100, chunkOverlap = 50)
+): List<String> =
+  HttpClient().use {
+    val response = it.get(url)
+    val file = File.createTempFile("pdf", ".pdf")
+    file.writeChannel().use {
+      response.bodyAsChannel().copyAndClose(this)
+    }
+    pdf(file, splitter)
+  }
+
 
 suspend fun pdf(
   file: File,
