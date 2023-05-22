1e9f712e1cbf769e42eb5cb2d32decdddf1ced6a
raulraja <raulraja@gmail.com>
2023-05-22 18:40:01 +0200

    merge main

--- filesystem/src/commonMain/kotlin/com/xebia/functional/xef/agents/CSV.kt
+++ filesystem/src/commonMain/kotlin/com/xebia/functional/xef/agents/CSV.kt
@@ -3,7 +3,7 @@ package com.xebia.functional.xef.agents
 import com.xebia.functional.tokenizer.ModelType
 import com.xebia.functional.xef.io.DEFAULT
 import com.xebia.functional.xef.loaders.CSVLoader
-import com.xebia.functional.xef.textsplitters.BaseTextSplitter
+import com.xebia.functional.xef.textsplitters.TextSplitter
 import com.xebia.functional.xef.textsplitters.TokenTextSplitter
 import okio.FileSystem
 import okio.Path
@@ -11,7 +11,7 @@ import okio.Path
 suspend fun csv(
   vararg files: Path,
   fileSystem: FileSystem = FileSystem.DEFAULT,
-  splitter: BaseTextSplitter = TokenTextSplitter(ModelType.GPT_3_5_TURBO, chunkSize = 100, chunkOverlap = 50)
+  splitter: TextSplitter = TokenTextSplitter(ModelType.GPT_3_5_TURBO, chunkSize = 100, chunkOverlap = 50)
 ): List<String> = files.flatMap { file ->
     val loader = CSVLoader(file, fileSystem)
     loader.loadAndSplit(splitter)
