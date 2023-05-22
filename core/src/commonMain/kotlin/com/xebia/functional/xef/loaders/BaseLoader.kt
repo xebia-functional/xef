382c27ffadf0cbe834279d42f46ee0ad56afd109
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-05-22 13:02:32 +0200

    Move TokenTextSplitter to commonMain (#85)

--- core/src/commonMain/kotlin/com/xebia/functional/xef/loaders/BaseLoader.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/loaders/BaseLoader.kt
@@ -1,9 +1,9 @@
 package com.xebia.functional.xef.loaders
 
-import com.xebia.functional.xef.textsplitters.BaseTextSplitter
+import com.xebia.functional.xef.textsplitters.TextSplitter
 
 interface BaseLoader {
   suspend fun load(): List<String>
-  suspend fun loadAndSplit(textSplitter: BaseTextSplitter): List<String> =
+  suspend fun loadAndSplit(textSplitter: TextSplitter): List<String> =
     textSplitter.splitDocuments(documents = load())
 }
