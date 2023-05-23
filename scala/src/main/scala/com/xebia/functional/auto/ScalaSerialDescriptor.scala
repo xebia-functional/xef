0f054063de1cdf03acc9d6d79afc576f11beb23a
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-05-23 10:32:38 +0200

    Add some descriptor implementations

--- scala/src/main/scala/com/xebia/functional/auto/ScalaSerialDescriptor.scala
+++ scala/src/main/scala/com/xebia/functional/auto/ScalaSerialDescriptor.scala
@@ -4,33 +4,29 @@ import java.lang.annotation.Annotation
 import kotlinx.serialization.descriptors.{SerialDescriptor, SerialKind, StructureKind}
 
 import scala.deriving.*
-import scala.compiletime.{constValue, erasedValue, summonInline}
+import scala.compiletime.{constValue, erasedValue}
 
 object ScalaSerialDescriptor:
   inline def derived[A <: Product](using m: Mirror.Of[A]): SerialDescriptor =
     new SerialDescriptor:
-      val serialName: String = constValue[m.MirroredLabel]
-      val kind: SerialKind = StructureKind.CLASS.INSTANCE
-      val elementsCount: Int = constValue[m.MirroredElemLabels].size
-
       def getElementIndex(name: String): Int = constValue[m.MirroredElemLabels].toArray.indexOf(name)
 
-      def getElementAnnotations(index: Int): java.util.List[Annotation] = ??? // You need to determine how to get the Annotations
+      def getElementAnnotations(index: Int): java.util.List[Annotation] = ???
 
       def getElementDescriptor(index: Int): SerialDescriptor = ??? // You need to determine how to get the SerialDescriptor
 
       override def getAnnotations: java.util.List[Annotation] = ???
 
-      override def getElementsCount: Int = ???
+      override def getElementsCount: Int = constValue[m.MirroredElemLabels].size
 
-      override def isInline: Boolean = ???
+      override def isInline: Boolean = false
 
-      override def isNullable: Boolean = ???
+      override def isNullable: Boolean = false
 
-      override def getKind: SerialKind = ???
+      override def getKind: SerialKind = StructureKind.CLASS.INSTANCE
 
-      override def getSerialName: String = ???
+      override def getSerialName: String = constValue[m.MirroredLabel]
 
-      override def getElementName(i: Int): String = ???
+      override def getElementName(i: Int): String = constValue[m.MirroredElemLabels].productElementName(i)
 
       def isElementOptional(index: Int): Boolean = false // You need to determine how to check if element is optional
