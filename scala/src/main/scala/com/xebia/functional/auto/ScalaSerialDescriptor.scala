426a04aee3de4b945828b1a2e37f062e4f182c59
Nelson Costa <nelson.costa@47deg.com>
2023-05-24 10:06:12 +0200

    Code review adjustments

--- scala/src/main/scala/com/xebia/functional/auto/ScalaSerialDescriptor.scala
+++ scala/src/main/scala/com/xebia/functional/auto/ScalaSerialDescriptor.scala
@@ -22,33 +22,33 @@ object ScalaSerialDescriptor:
     case _: EmptyTuple => Nil
     case _: (h *: t) => erasedValue[h].toString :: getElemsLabel[t]
 
-  private inline def getElemTypes[T <: Tuple]: List[SerialDescriptor] = inline erasedValue[T] match
+  private inline def getSerialDescriptor[T <: Tuple]: List[SerialDescriptor] = inline erasedValue[T] match
     case _: EmptyTuple => Nil
     case _: (h *: t) =>
-      summonInline[ClassTag[h]].runtimeClass.toString match {
-        case s if s.toLowerCase.contains("boolean") => KotlinXSerializers.boolean.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("byte") => KotlinXSerializers.byte.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("char") => KotlinXSerializers.char.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("double") => KotlinXSerializers.double.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("float") => KotlinXSerializers.float.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("int") => KotlinXSerializers.int.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("long") => KotlinXSerializers.long.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("short") => KotlinXSerializers.short.getDescriptor :: getElemTypes[t]
-        case s if s.toLowerCase.contains("string") => KotlinXSerializers.string.getDescriptor :: getElemTypes[t]
-        case _ => summonInline[ScalaSerialDescriptor[h]].serialDescriptor :: getElemTypes[t]
+      summonInline[ClassTag[h]].runtimeClass match {
+        case x if classOf[Boolean].isAssignableFrom(x) => KotlinXSerializers.boolean.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[Byte].isAssignableFrom(x) => KotlinXSerializers.byte.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[Char].isAssignableFrom(x) => KotlinXSerializers.char.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[Double].isAssignableFrom(x) => KotlinXSerializers.double.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[Float].isAssignableFrom(x) => KotlinXSerializers.float.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[Int].isAssignableFrom(x) => KotlinXSerializers.int.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[Long].isAssignableFrom(x) => KotlinXSerializers.long.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[Short].isAssignableFrom(x) => KotlinXSerializers.short.getDescriptor :: getSerialDescriptor[t]
+        case x if classOf[String].isAssignableFrom(x) => KotlinXSerializers.string.getDescriptor :: getSerialDescriptor[t]
+        case _ => summonInline[ScalaSerialDescriptor[h]].serialDescriptor :: getSerialDescriptor[t]
       }
 
   inline final def derived[A](using inline m: Mirror.Of[A]): ScalaSerialDescriptor[A] = new ScalaSerialDescriptor[A]:
     val serialDescriptorImpl: SerialDescriptor = new SerialDescriptor:
       val labels = getElemsLabel[m.MirroredElemLabels]
-      val types = getElemTypes[m.MirroredElemTypes]
+      val serialDescriptors = getSerialDescriptor[m.MirroredElemTypes]
 
       override def getElementIndex(name: String): Int = labels.indexOf(name)
 
       // We're going to ignore annotations for now, it's not relevant for JsonSchema
       override def getElementAnnotations(index: Int): util.List[Annotation] = java.util.ArrayList(0)
 
-      override def getElementDescriptor(index: Int): SerialDescriptor = types(index)
+      override def getElementDescriptor(index: Int): SerialDescriptor = serialDescriptors(index)
 
       // We're going to ignore annotations for now, it's not relevant for JsonSchema
       override def getAnnotations: util.List[Annotation] = java.util.ArrayList(0)
