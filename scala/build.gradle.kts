775c39a9b73f7f38e86876f03952a8c5805feed7
franciscodr <francisco.d@47deg.com>
2023-05-26 15:52:36 +0200

    Add POM and signing configuration

--- scala/build.gradle.kts
+++ scala/build.gradle.kts
@@ -40,8 +40,46 @@ publishing {
             from(components["java"])
 
             artifactId = base.archivesName.get() + scala3Suffix
+
+            pom {
+                name.set(project.properties["pom.name"]?.toString())
+                description.set(project.properties["pom.description"]?.toString())
+                url.set(project.properties["pom.url"]?.toString())
+
+                licenses {
+                    license {
+                        name.set(project.properties["pom.license.name"]?.toString())
+                        url.set(project.properties["pom.license.url"]?.toString())
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set(project.properties["pom.developer.id"].toString())
+                        name.set(project.properties["pom.developer.name"].toString())
+                    }
+                }
+
+                scm {
+                    url.set(project.properties["pom.smc.url"].toString())
+                    connection.set(project.properties["pom.smc.connection"].toString())
+                    developerConnection.set(project.properties["pom.smc.developerConnection"].toString())
+                }
+            }
         }
     }
+}
+
+signing {
+    val isLocal = gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal", ignoreCase = true) }
+    val signingKeyId: String? = System.getenv("SIGNING_KEY_ID")
+    val signingKey: String? = System.getenv("SIGNING_KEY")
+    val signingPassphrase: String? = System.getenv("SIGNING_KEY_PASSPHRASE")
+
+    isRequired = !isLocal
+    useGpgCmd()
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassphrase)
+    sign(publishing.publications)
 }
 
 spotless {
