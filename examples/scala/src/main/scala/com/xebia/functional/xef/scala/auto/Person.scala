b11ea1fc4eea059c5f3d2ac7f603a2f1b6249c77
Nelson Costa <nelson.costa@47deg.com>
2023-05-30 12:19:23 +0200

    Fixed Fact and Person examples

--- examples/scala/src/main/scala/com/xebia/functional/xef/scala/auto/Person.scala
+++ examples/scala/src/main/scala/com/xebia/functional/xef/scala/auto/Person.scala
@@ -1,6 +1,7 @@
 package com.xebia.functional.xef.scala.auto
 
 import com.xebia.functional.xef.scala.auto.*
+import com.xebia.functional.xef.scala.auto.ScalaSerialDescriptorContext.given
 import io.circe.Decoder
 import io.circe.parser.decode
 
@@ -8,6 +9,6 @@ private final case class Person(name: String, age: Int) derives ScalaSerialDescr
 
 @main def runPerson: Unit =
   ai {
-    val person = prompt[Person]("What is your name and age?")
+    val person = prompt[Person]("Hello made-up person, what is your name and age?")
     println(s"Hello ${person.name}, you are ${person.age} years old.")
   }.getOrElse(ex => println(ex.getMessage))
