d98b780e01a9e8148a412fb56a32d51779bf32bc
yago <Yawolf@users.noreply.github.com>
2023-05-31 16:02:34 +0200

    Refined

--- scala/src/main/scala/com/xebia/functional/xef/scala/textsplitters/TextSplitter.scala
+++ scala/src/main/scala/com/xebia/functional/xef/scala/textsplitters/TextSplitter.scala
@@ -4,33 +4,24 @@ import com.xebia.functional.loom.LoomAdapter
 import com.xebia.functional.xef.textsplitters.TextSplitter as KtTextSplitter
 import com.xebia.functional.xef.textsplitters.{CharacterTextSplitterKt, TokenTextSplitterKt}
 import com.xebia.functional.tokenizer.ModelType
+import kotlin.coroutines.Continuation
 
 import scala.jdk.CollectionConverters.*
+import scala.util.Try
 
-abstract class TextSplitter private (kt: KtTextSplitter):
-  def core: KtTextSplitter = kt
-  def splitText(text: String): List[String]
-  def splitDocuments(documents: List[String]): List[String]
-  def splitTextInDocuments(text: String): List[String]
+trait TextSplitter:
+  private type Ret = java.util.List[String]
+  def core: KtTextSplitter
+  def splitText(text: String): List[String] =
+    LoomAdapter.apply[Ret](core.splitText(text, _)).asScala.toList
+  def splitDocuments(documents: List[String]): List[String] =
+    LoomAdapter.apply[Ret](core.splitDocuments(documents.asJava, _)).asScala.toList
+  def splitTextInDocuments(text: String): List[String] =
+    LoomAdapter.apply[Ret](core.splitTextInDocuments(text, _)).asScala.toList
 
 object TextSplitter:
-  private type Ret = java.util.List[String]
-  def characterTextSplitter(separator: String): TextSplitter =
-    val kt = CharacterTextSplitterKt.CharacterTextSplitter(separator)
-    new TextSplitter(kt):
-      def splitText(text: String): List[String] =
-        LoomAdapter.apply[Ret](kt.splitText(text, _)).asScala.toList
-      def splitDocuments(documents: List[String]): List[String] =
-        LoomAdapter.apply[Ret](kt.splitDocuments(documents.asJava, _)).asScala.toList
-      def splitTextInDocuments(text: String): List[String] =
-        LoomAdapter.apply[Ret](kt.splitTextInDocuments(text, _)).asScala.toList
+  def characterTextSplitter(separator: String): TextSplitter = new TextSplitter:
+    def core: KtTextSplitter = CharacterTextSplitterKt.CharacterTextSplitter(separator)
 
-  def tokenTextSplitter(modelType: ModelType, chunkSize: Int, chunkOverlap: Int): TextSplitter =
-    val kt = TokenTextSplitterKt.TokenTextSplitter(modelType, chunkSize, chunkOverlap)
-    new TextSplitter(kt):
-      override def splitText(text: String): List[String] =
-        LoomAdapter.apply[Ret](kt.splitText(text, _)).asScala.toList
-      override def splitDocuments(documents: List[String]): List[String] =
-        LoomAdapter.apply[Ret](kt.splitDocuments(documents.asJava, _)).asScala.toList
-      override def splitTextInDocuments(text: String): List[String] =
-        LoomAdapter.apply[Ret](kt.splitTextInDocuments(text, _)).asScala.toList
+  def tokenTextSplitter(modelType: ModelType, chunkSize: Int, chunkOverlap: Int): TextSplitter = new TextSplitter:
+    def core: KtTextSplitter = TokenTextSplitterKt.TokenTextSplitter(modelType, chunkSize, chunkOverlap)
