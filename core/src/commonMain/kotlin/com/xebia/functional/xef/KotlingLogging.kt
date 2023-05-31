1efb2d424d64a0dca04ed23abaf556ba7004f83a
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-05-31 09:53:35 +0200

    Removes example from core, and unused function

--- core/src/commonMain/kotlin/com/xebia/functional/xef/KotlingLogging.kt
+++
@@ -1,7 +0,0 @@
-package com.xebia.functional.xef
-
-import io.github.oshai.KLogger
-
-fun KLogger.logTruncated(ctx: String, msg: String, max: Int = 100): Unit = debug {
-  if (msg.length > max) "[$ctx] ${msg.take(max)}..." else "[$ctx] $msg"
-}
