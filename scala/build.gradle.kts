05b9b7a22930a871f9eecf5066e87261d616eb15
Nelson Costa <nelson.costa@47deg.com>
2023-05-31 09:13:21 +0200

    Enable Scala cross compilation

--- scala/build.gradle.kts
+++ scala/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     scala
     `maven-publish`
     signing
+    id("com.github.prokod.gradle-crossbuild").version("0.15.0")
     alias(libs.plugins.semver.gradle)
     alias(libs.plugins.spotless)
 }
@@ -35,37 +36,54 @@ tasks.withType<Test>().configureEach {
     useJUnit()
 }
 
+crossBuild {
+    scalaVersionsCatalog = mapOf("3" to "3.2.2", "2.13" to "2.13.10")
+    builds {
+        register("scala") {
+            scalaVersions = setOf("3", "2.13")
+        }
+    }
+}
+
+val crossBuildScala_3Jar by tasks.getting
+val crossBuildScala_213Jar by tasks.getting
+
 publishing {
     publications {
-        create<MavenPublication>("maven") {
-            val scala3Suffix = "_3"
-            from(components["java"])
+        mapOf(
+            "3" to crossBuildScala_3Jar,
+            "2.13" to crossBuildScala_213Jar
+        ).forEach { (scalaVersion, versionedClassJar) ->
+            create<MavenPublication>("maven_$scalaVersion") {
+                val scalaSuffix = "_${scalaVersion.replace(".", "")}"
+                from(components["java"])
 
-            artifactId = base.archivesName.get() + scala3Suffix
+                artifactId = base.archivesName.get() + scalaSuffix
 
-            pom {
-                name.set(project.properties["pom.name"]?.toString())
-                description.set(project.properties["pom.description"]?.toString())
-                url.set(project.properties["pom.url"]?.toString())
+                pom {
+                    name.set(project.properties["pom.name"]?.toString())
+                    description.set(project.properties["pom.description"]?.toString())
+                    url.set(project.properties["pom.url"]?.toString())
 
-                licenses {
-                    license {
-                        name.set(project.properties["pom.license.name"]?.toString())
-                        url.set(project.properties["pom.license.url"]?.toString())
+                    licenses {
+                        license {
+                            name.set(project.properties["pom.license.name"]?.toString())
+                            url.set(project.properties["pom.license.url"]?.toString())
+                        }
                     }
-                }
 
-                developers {
-                    developer {
-                        id.set(project.properties["pom.developer.id"].toString())
-                        name.set(project.properties["pom.developer.name"].toString())
+                    developers {
+                        developer {
+                            id.set(project.properties["pom.developer.id"].toString())
+                            name.set(project.properties["pom.developer.name"].toString())
+                        }
                     }
-                }
 
-                scm {
-                    url.set(project.properties["pom.smc.url"].toString())
-                    connection.set(project.properties["pom.smc.connection"].toString())
-                    developerConnection.set(project.properties["pom.smc.developerConnection"].toString())
+                    scm {
+                        url.set(project.properties["pom.smc.url"].toString())
+                        connection.set(project.properties["pom.smc.connection"].toString())
+                        developerConnection.set(project.properties["pom.smc.developerConnection"].toString())
+                    }
                 }
             }
         }
