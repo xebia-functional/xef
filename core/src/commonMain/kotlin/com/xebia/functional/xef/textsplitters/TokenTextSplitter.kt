1ad3934a98cc1562bdda516bc76bb48b520392e8
Diego E. Alonso <diego.e.a@47deg.com>
2023-06-23 13:00:22 +0100

    TokenTextSplitter Ref: extract method, use range. (#198)
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>

--- core/src/commonMain/kotlin/com/xebia/functional/xef/textsplitters/TokenTextSplitter.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/textsplitters/TokenTextSplitter.kt
@@ -14,14 +14,12 @@ private class TokenTextSplitterImpl(
 
   override suspend fun splitText(text: String): List<String> {
     val inputIds = tokenizer.encode(text)
-    val stepSize = chunkSize - chunkOverlap
+    fun decodeSegment(startIdx: Int): String {
+      val end = minOf(startIdx + chunkSize, inputIds.size)
+      return tokenizer.decode(inputIds.subList(startIdx, end))
+    }
 
-    return inputIds.indices
-      .asSequence()
-      .filter { it % stepSize == 0 }
-      .map { startIdx -> inputIds.subList(startIdx, minOf(startIdx + chunkSize, inputIds.size)) }
-      .map { chunkIds -> tokenizer.decode(chunkIds) }
-      .toList()
+    return inputIds.indices.step(chunkSize - chunkOverlap).map { decodeSegment(it) }
   }
 
   override suspend fun splitDocuments(documents: List<String>): List<String> =
