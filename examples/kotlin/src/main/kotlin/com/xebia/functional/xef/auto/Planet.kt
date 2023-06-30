8ed9354a8f0de6c1c7f2494f675658445b3c184f
Adam Kucera <120064571+bitgama5@users.noreply.github.com>
2023-06-30 00:18:13 -0700

    Update Planet.kt with units in var names

--- examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/Planet.kt
+++ examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/Planet.kt
@@ -3,10 +3,10 @@ package com.xebia.functional.xef.auto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Planet(val name: String, val distanceFromSun: Double, val moons: List<Moon>)
+data class Planet(val name: String, val distanceFromSunInMillionKm: Double, val moons: List<Moon>)
 
 @Serializable
-data class Moon(val name: String, val distanceFromPlanet: Double)
+data class Moon(val name: String, val distanceFromPlanetInKm: Double)
 
 suspend fun main() =
     ai {
@@ -14,9 +14,9 @@ suspend fun main() =
       val mars: Planet = prompt("Information about Mars and its moons.")
 
       fun planetInfo(planet: Planet): String {
-        return """${planet.name} is ${planet.distanceFromSun} million km away from the Sun.
+        return """${planet.name} is ${planet.distanceFromSunInMillionKm} million km away from the Sun.
               |It has the following moons:
-              |${planet.moons.joinToString("\n") { "  - ${it.name}: ${it.distanceFromPlanet} km away from ${planet.name}" }}
+              |${planet.moons.joinToString("\n") { "  - ${it.name}: ${it.distanceFromPlanetInKm} km away from ${planet.name}" }}
               """.trimMargin()
       }
 
