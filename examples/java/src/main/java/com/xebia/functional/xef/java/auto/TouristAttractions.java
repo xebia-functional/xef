766876d006b910d1306d6b45777321e61b26e0a5
Adam Kucera <120064571+bitgama5@users.noreply.github.com>
2023-06-30 00:19:28 -0700

    Add TouristAttractions.java examples

---
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/TouristAttractions.java
@@ -0,0 +1,23 @@
+package com.xebia.functional.xef.java.auto;
+
+import java.util.concurrent.ExecutionException;
+
+public class TouristAttractions {
+
+    static class TouristAttraction {
+        public String name;
+        public String location;
+        public String history;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try (AIScope scope = new AIScope()) {
+            scope.prompt("Statue of Liberty location and history.", TouristAttractions.TouristAttraction.class)
+                    .thenAccept((statueOfLiberty) -> System.out.println(
+                            statueOfLiberty.name + "is located in " + statueOfLiberty.location +
+                                    " and has the following history: " + statueOfLiberty.history
+                            )
+                    ).get();
+        }
+    }
+}
