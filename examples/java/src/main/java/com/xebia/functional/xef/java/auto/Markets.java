41934dde083d2da9ca2072c0bbd740c1d6c88360
Daniel Gelvez <danielgelvez.18@gmail.com>
2023-06-30 20:48:53 -0500

    Add examples

---
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/Markets.java
@@ -0,0 +1,24 @@
+package com.xebia.functional.xef.java.auto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class Markets {
+    public String news;
+    public List<String> raisingStockSymbols;
+    public List<String> decreasingStockSymbols;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try (AIScope scope = new AIScope()) {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/M/yyyy");
+            LocalDateTime now = LocalDateTime.now();
+            String news = "|" +
+                  "|Write a short summary of the stock market results given the provided context.";
+            scope.prompt(news, Markets.class)
+                  .thenAccept(markets -> System.out.println(markets))
+                  .get();
+        }
+    }
+}
---
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/MealPlan.java
@@ -0,0 +1,21 @@
+package com.xebia.functional.xef.java.auto;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class MealPlan {
+    public String name;
+    public List<Recipe> recipes;
+
+    private static class Recipe {
+        public String name;
+        public List<String> ingredients;
+    }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try (AIScope scope = new AIScope()) {
+            scope.prompt("Meal plan for the week for a person with gall bladder stones that includes 5 recipes.", MealPlan.class)
+                  .thenAccept(mealPlan -> System.out.println(mealPlan))
+                  .get();
+        }
+    }
+}
