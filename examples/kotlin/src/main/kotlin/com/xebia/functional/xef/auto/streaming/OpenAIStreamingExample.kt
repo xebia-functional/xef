b2bbf87ee044d183f98bcfeb6fb0d507515a19ff
raulraja <raulraja@gmail.com>
2023-07-03 19:48:02 +0200

    OpenAI example

--- examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/streaming/OpenAIStreamingExample.kt
+++ examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/streaming/OpenAIStreamingExample.kt
@@ -9,13 +9,11 @@ suspend fun main() {
   val chat: Chat = OpenAI.DEFAULT_CHAT
   val embeddings = OpenAIEmbeddings(OpenAI.DEFAULT_EMBEDDING)
   val vectorStore = LocalVectorStore(embeddings)
-  // hack until https://github.com/nomic-ai/gpt4all/pull/1126 is accepted or merged
-  val out = System.out
   chat.promptStreaming(
     question = "What is the meaning of life?",
     context = vectorStore
   ).collect {
-    out.print(it)
+    print(it)
   }
 }
 
