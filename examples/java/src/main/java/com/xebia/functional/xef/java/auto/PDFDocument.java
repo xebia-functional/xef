af8c6e1094928a4332fb69c7e8ac8dc31bea256f
Adam Kucera <120064571+bitgama5@users.noreply.github.com>
2023-07-03 20:45:00 -0700

    Use default model type, System.in as console input in PDFDocument.java

--- examples/java/src/main/java/com/xebia/functional/xef/java/auto/PDFDocument.java
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/PDFDocument.java
@@ -3,6 +3,9 @@ package com.xebia.functional.xef.java.auto;
 import com.xebia.functional.tokenizer.ModelType;
 import com.xebia.functional.xef.textsplitters.TextSplitter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -16,14 +19,15 @@ public class PDFDocument {
     }
 
     private static final String pdfUrl = "https://people.cs.ksu.edu/~schmidt/705a/Scala/Programming-in-Scala.pdf";
+    private static final BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
 
     private static CompletableFuture<Void> askQuestion(AIScope scope) {
         System.out.println("Enter your question: ");
-        String line = System.console().readLine();
-        if (line == null) {
+
+        if (readLine() == null) {
             return CompletableFuture.completedFuture(null);
         } else {
-            scope.prompt(line, AIResponse.class)
+            scope.prompt(readLine(), AIResponse.class)
                     .thenAccept((aiRes) -> System.out.println(aiRes.answer + "\n---\n" +
                             aiRes.source + "\n---\n"));
 
@@ -31,10 +35,18 @@ public class PDFDocument {
         }
     }
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        TextSplitter textSplitter = TokenTextSplitter(ModelType.GPT_3_5_TURBO, 100, 50);
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+        TextSplitter textSplitter = TokenTextSplitter(ModelType.getDEFAULT_SPLITTER_MODEL(), 100, 50);
         try (AIScope scope = new AIScope()) {
-            scope.contextScope(scope.pdf(pdfUrl, textSplitter), PDFDocument::askQuestion).get();
+            scope.contextScope(scope.pdf(pdfUrl, textSplitter).get(), PDFDocument::askQuestion).get();
+        }
+    }
+
+    private static String readLine() {
+        try {
+            return sysin.readLine();
+        } catch (IOException e) {
+            return null;
         }
     }
 }
