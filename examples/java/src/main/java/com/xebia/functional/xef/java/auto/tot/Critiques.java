792ccc40d9e71f1aa90803ec83f8184effc507ba
Adam Kucera <120064571+bitgama5@users.noreply.github.com>
2023-07-05 23:15:49 -0700

    Add Critiques.java for tot

---
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/tot/Critiques.java
@@ -0,0 +1,16 @@
+package com.xebia.functional.xef.java.auto.tot;
+
+public class Critiques {
+
+    static class Critique {
+        String answer;
+        String reasoning;
+        boolean answerTrulyAccomplishesTheGoal;
+    }
+
+    public <A> Critique critique(Problems.Memory<A> memory, Solutions.Solution<A> solution){
+
+        //TODO
+        return null;
+    }
+}
