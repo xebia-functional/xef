c0a5934f8bb627f19bf69bd62b92bbbee80faefc
Adam Kucera <120064571+bitgama5@users.noreply.github.com>
2023-07-06 20:31:11 -0700

    Complete Rendering.java with history and add null check

--- examples/java/src/main/java/com/xebia/functional/xef/java/auto/tot/Rendering.java
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/tot/Rendering.java
@@ -1,15 +1,32 @@
 package com.xebia.functional.xef.java.auto.tot;
 
+import java.util.stream.Collectors;
+
 public class Rendering {
 
     public static String truncateText(String answer) {
         return truncateText(answer, 150);
     }
 
-    public static String truncateText(String answer, int i) {
-        if(answer != null && answer.length() > i) {
-            return answer.substring(0, i);
+    public static String truncateText(String answer, int limit) {
+        if(answer == null)
+            return "Empty answer";
+        if(answer.length() > limit) {
+            answer = answer.substring(0, limit - 3) + "...";
         }
-        return answer;
+        return answer.replace("\n", " ");
+    }
+
+    public static <A> String renderHistory(Problems.Memory<A> memory){
+        return "   history \n\n" +
+        memory.history.stream()
+                .map(it -> renderHistoryItem(it))
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static <S> String renderHistoryItem(Solutions.Solution<S> solution){
+        return solution.answer + "\n" +
+                solution.reasoning + "\n" +
+                (solution.isValid ? "✅" : "❌") + "\n";
     }
 }
