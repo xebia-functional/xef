79979f44674a46ce65c614ee6c708f5af4070aec
Juan Pedro Moreno <4879373+juanpedromoreno@users.noreply.github.com>
2023-07-07 13:55:29 +0200

    Redefines the Gradle build strategy (#175)
    * Redefines the Gradle build strategy
    
    Only js platform
    
    Platform is not necessary for single-platform
    
    Unifies gradlew commands
    
    Removes macosarm64
    
    Improves build commands
    
    on pull_request
    
    * Runs-on based on matrix. It also
    check the code formatting including a new step
    
    * gradlew VW gradelw.bat
    
    * Fixes build.yml file
    
    * Changes gradlew.bat permissions
    
    * cmd.exe /c
    
    * + task aliases
    
    * Windows gradlecommand just gradle.bat
    
    * quoting windows command
    
    * Further optimizations
    
    * Fixes a bug
    
    * Fixes a bug trying to fix another bug
    
    * Singlep only for jvm
    
    * Getting list of subprojects dynamically
    
    * Filtering multiplatform
    
    ---------
    
    Co-authored-by: Raúl Raja Martínez <raulraja@gmail.com>
    Co-authored-by: Diego E. Alonso <diego.e.a@47deg.com>
    Co-authored-by: yago <Yawolf@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,66 @@ plugins {
 allprojects {
   group = property("project.group").toString()
 }
+
+fun isMultiplatformModule(project: Project): Boolean {
+  val kotlinPluginId = "libs.plugins.kotlin.multiplatform"
+  return project.buildFile.readText().contains(kotlinPluginId)
+}
+
+val multiPlatformModules = project.subprojects.filter { isMultiplatformModule(it) }.map { it.name }
+
+enum class ModuleType {
+  MULTIPLATFORM,
+  SINGLEPLATFORM
+}
+
+fun Project.configureBuildAndTestTask(
+  taskName: String,
+  moduleType: ModuleType,
+  multiPlatformModules: List<String>
+) {
+  val platform: String by extra
+
+  tasks.register(taskName) {
+    doLast {
+      project.exec {
+        val gradleCommand = getGradleCommand(platform)
+        commandLine(gradleCommand, "spotlessCheck")
+        when (moduleType) {
+          ModuleType.MULTIPLATFORM -> {
+            multiPlatformModules.forEach { module ->
+              commandLine(gradleCommand, ":$module:${platform}Test")
+            }
+          }
+          ModuleType.SINGLEPLATFORM -> {
+            commandLine(gradleCommand, "build", *buildExcludeOptions(multiPlatformModules))
+          }
+        }
+      }
+    }
+  }
+}
+
+fun Project.buildExcludeOptions(modules: List<String>): Array<String> {
+  return modules.flatMap { listOf("-x", ":$it:build") }.toTypedArray()
+}
+
+fun getGradleCommand(platform: String): String {
+  return if (platform == "mingwX64") {
+    "gradlew.bat"
+  } else {
+    "./gradlew"
+  }
+}
+
+configureBuildAndTestTask(
+  "buildAndTestMultip",
+  ModuleType.MULTIPLATFORM,
+  multiPlatformModules
+)
+
+configureBuildAndTestTask(
+  "buildAndTestSinglep",
+  ModuleType.SINGLEPLATFORM,
+  multiPlatformModules
+)
