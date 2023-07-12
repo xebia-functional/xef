774adb269cd1da537430be2b9de4e064dbe5e8a0
Adam Kucera <120064571+bitgama5@users.noreply.github.com>
2023-07-12 10:49:10 -0700

    Adding toString() to data classes for cleaner println

--- examples/java/src/main/java/com/xebia/functional/xef/java/auto/Movies.java
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/Movies.java
@@ -8,13 +8,18 @@ public class Movies {
         public String title;
         public String genre;
         public String director;
+
+        @Override
+        public String toString() {
+            return "The movie " + title + " is a " +
+                    genre + " film directed by " + director + ".";
+        }
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (AIScope scope = new AIScope()) {
             scope.prompt("Please provide a movie title, genre and director for the Inception movie", Movie.class)
-                    .thenAccept(movie -> System.out.println("The movie " + movie.title + " is a " +
-                            movie.genre + " film directed by " + movie.director + "."))
+                    .thenAccept(movie -> System.out.println(movie))
                     .get();
         }
     }
--- examples/java/src/main/java/com/xebia/functional/xef/java/auto/Persons.java
+++ examples/java/src/main/java/com/xebia/functional/xef/java/auto/Persons.java
@@ -7,12 +7,17 @@ public class Persons {
     public static class Person {
         public String name;
         public int age;
+
+        @Override
+        public String toString() {
+            return "Hello " + name + ", you are " + age + " years old.";
+        }
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (AIScope scope = new AIScope()) {
             scope.prompt("What is your name and age?", Person.class)
-                    .thenAccept(person -> System.out.println("Hello " + person.name + ", you are " + person.age + " years old."))
+                    .thenAccept(person -> System.out.println(person))
                     .get();
         }
     }
