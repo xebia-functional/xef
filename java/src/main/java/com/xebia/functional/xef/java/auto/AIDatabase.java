8826e51d46e95bcd622e3fc00ddc62f55521d833
Adam Kucera <120064571+bitgama5@users.noreply.github.com>
2023-07-19 22:15:53 -0700

    Updates to AIDatabase to use SharedExecution Complete methods

--- java/src/main/java/com/xebia/functional/xef/java/auto/AIDatabase.java
+++ java/src/main/java/com/xebia/functional/xef/java/auto/AIDatabase.java
@@ -1,28 +1,34 @@
 package com.xebia.functional.xef.java.auto;
 
+import com.xebia.functional.xef.auto.CoreAIScope;
 import com.xebia.functional.xef.sql.SQL;
 import com.xebia.functional.xef.sql.jdbc.JdbcConfig;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class AIDatabase {
-    private AIScope scope;
-    private JdbcConfig jdbcConfig;
+    private final CoreAIScope scope;
     private SQL sql;
+    private SharedExecution exec;
 
-    public AIDatabase(AIScope scope, JdbcConfig jdbcConfig) {
-        this.scope = scope;
-        this.jdbcConfig = jdbcConfig;
+    public AIDatabase(JdbcConfig jdbcConfig, SharedExecution sharedExecution) {
         sql = SQL.Companion.fromJdbcConfigSync(jdbcConfig);
+        this.exec = sharedExecution;
+        this.scope = sharedExecution.getCoreScope();
     }
 
     public CompletableFuture<String> getInterestingPromptsForDatabase() {
-
-        return scope.getInterestingPromptsForDatabase(sql);
+        return exec.future(continuation -> sql.getInterestingPromptsForDatabase(scope, continuation));
     }
 
-
-    public Object promptMessage(String question, Object context, Object o1, Object o2, Object o3) {
-        return null;
+    public void extendContext(List<String> textsToAdd) {
+        String[] textsArray = textsToAdd.toArray(new String[textsToAdd.size()]);
+        exec.future(continuation -> scope.extendContext(textsArray, continuation));
     }
+
+    public CompletableFuture<List<String>> promptQuery(String input) {
+        return exec.future(continuation -> sql.promptQuery(scope, input, continuation));
+    }
+
 }
