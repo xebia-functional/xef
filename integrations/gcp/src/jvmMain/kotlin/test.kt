77ff5618dace139f09f8ce8844d6e5499f925381
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-07-26 10:37:24 +0200

    Implement GCP Ktor Client

---
+++ integrations/gcp/src/jvmMain/kotlin/test.kt
@@ -0,0 +1,11 @@
+import com.xebia.functional.xef.gcp.GcpClient
+
+suspend fun main() {
+  val apiEndpoint = "us-central1-aiplatform.googleapis.com"
+  val projectId = "xefdemo"
+  val modelId = "codechat-bison@001"
+  val token = "TOKEN"
+  GcpClient(apiEndpoint, projectId, modelId, token).use {
+    it.promptMessage("How can I reverse a list in python?",).also(::println)
+  }
+}
