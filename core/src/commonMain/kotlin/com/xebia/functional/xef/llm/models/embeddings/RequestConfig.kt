042db731cd657fd89deaa4144bc53e8a2ab67dfe
Sören Brunk <soeren@brunk.io>
2023-07-31 13:59:10 +0200

    Add static apply method to enable creating RequestConfig from Scala (#285)
    * Add static apply method to enable creating RequestConfig from Scala
    
    Fixes #275
    
    * `./gradlew spotlessApply`
    
    ---------
    
    Co-authored-by: Raúl Raja Martínez <raulraja@gmail.com>

--- core/src/commonMain/kotlin/com/xebia/functional/xef/llm/models/embeddings/RequestConfig.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/llm/models/embeddings/RequestConfig.kt
@@ -1,9 +1,15 @@
 package com.xebia.functional.xef.llm.models.embeddings
 
 import kotlin.jvm.JvmInline
+import kotlin.jvm.JvmStatic
 
 data class RequestConfig(val model: EmbeddingModel, val user: User) {
   companion object {
+    @JvmStatic
+    fun apply(model: EmbeddingModel, userId: String): RequestConfig {
+      return RequestConfig(model, User(userId))
+    }
+
     @JvmInline value class User(val id: String)
   }
 }
