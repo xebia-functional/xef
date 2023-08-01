bce62f79b55549abc6eb13536a70151ecefc5b5e
Daniel Gelvez <danielgelvez.18@gmail.com>
2023-08-01 17:12:36 -0500

    Format

--- reasoning/src/commonMain/kotlin/com/xebia/functional/xef/reasoning/serpapi/SerpApiClient.kt
+++ reasoning/src/commonMain/kotlin/com/xebia/functional/xef/reasoning/serpapi/SerpApiClient.kt
@@ -14,67 +14,76 @@ import kotlinx.serialization.json.Json
 
 class SerpApiClient : AutoCloseable {
 
-    private val serpApiKey: String?
-    private val SERP_API_KEY_NOT_FOUND = "Missing SERP_API_KEY env var"
+  private val serpApiKey: String?
+  private val SERP_API_KEY_NOT_FOUND = "Missing SERP_API_KEY env var"
 
-    init {
-        serpApiKey = getenv("SERP_API_KEY") ?: throw SerpApiClientException(HttpStatusCode.Unauthorized, SERP_API_KEY_NOT_FOUND)
+  init {
+    serpApiKey =
+      getenv("SERP_API_KEY")
+        ?: throw SerpApiClientException(HttpStatusCode.Unauthorized, SERP_API_KEY_NOT_FOUND)
 
-        if(serpApiKey.isEmpty())
-            throw SerpApiClientException(HttpStatusCode.Unauthorized, SERP_API_KEY_NOT_FOUND)
+    if (serpApiKey.isEmpty())
+      throw SerpApiClientException(HttpStatusCode.Unauthorized, SERP_API_KEY_NOT_FOUND)
+  }
+
+  private val http: HttpClient = HttpClient {
+    install(HttpTimeout) {
+      requestTimeoutMillis = 60_000
+      connectTimeoutMillis = 60_000
     }
-
-    private val http: HttpClient = HttpClient {
-        install(HttpTimeout) {
-            requestTimeoutMillis = 60_000
-            connectTimeoutMillis = 60_000
+    install(HttpRequestRetry) {
+      maxRetries = 5
+      retryIf { _, response -> !response.status.isSuccess() }
+      retryOnExceptionIf { _, _ -> true }
+      delayMillis { retry -> retry * 3000L }
+    }
+    install(ContentNegotiation) {
+      json(
+        Json {
+          encodeDefaults = false
+          isLenient = true
+          ignoreUnknownKeys = true
         }
-        install(HttpRequestRetry) {
-            maxRetries = 5
-            retryIf { _, response -> !response.status.isSuccess() }
-            retryOnExceptionIf { _, _ -> true }
-            delayMillis { retry -> retry * 3000L }
-        }
-        install(ContentNegotiation) {
-            json(
-                Json {
-                    encodeDefaults = false
-                    isLenient = true
-                    ignoreUnknownKeys = true
-                }
-            )
-        }
+      )
     }
+  }
 
-    data class SearchData(
-        val search: String,
-        val location: String? = null,
-        val language: String? = null,
-        val region: String? = null,
-        val googleDomain: String = "google.com"
-    )
+  data class SearchData(
+    val search: String,
+    val location: String? = null,
+    val language: String? = null,
+    val region: String? = null,
+    val googleDomain: String = "google.com"
+  )
 
-    @Serializable data class SearchResults(@SerialName("organic_results") val searchResults: List<SearchResult>)
-    @Serializable data class SearchResult(
-        val title: String,
-        @SerialName("snippet") val document: String,
-        @SerialName("link") val source: String
-    )
+  @Serializable
+  data class SearchResults(@SerialName("organic_results") val searchResults: List<SearchResult>)
 
-    suspend fun search(searchData: SearchData): SearchResults {
+  @Serializable
+  data class SearchResult(
+    val title: String,
+    @SerialName("snippet") val document: String,
+    @SerialName("link") val source: String
+  )
 
-        return http.get(
-                "https://serpapi.com/search.json?q=${searchData.search}&location=${searchData.location}&hl=${searchData.language}&gl=${searchData.region}" +
-                        "&google_domain=${searchData.googleDomain}&api_key=${serpApiKey}"
-            ) {
-                contentType(ContentType.Application.Json)
-            }.body<SearchResults>()
-    }
+  suspend fun search(searchData: SearchData): SearchResults {
 
-    class SerpApiClientException(private val httpStatusCode: HttpStatusCode, private val error: String) :
-        IllegalStateException("$httpStatusCode: $error")
+    return http
+      .get(
+        "https://serpapi.com/search.json?q=${searchData.search}&location=${searchData.location}&hl=${searchData.language}&gl=${searchData.region}" +
+          "&google_domain=${searchData.googleDomain}&api_key=${serpApiKey}"
+      ) {
+        contentType(ContentType.Application.Json)
+      }
+      .body<SearchResults>()
+  }
 
-    override fun close() {
-        http.close()
-    }
+  class SerpApiClientException(
+    private val httpStatusCode: HttpStatusCode,
+    private val error: String
+  ) : IllegalStateException("$httpStatusCode: $error")
+
+  override fun close() {
+    http.close()
+  }
 }
