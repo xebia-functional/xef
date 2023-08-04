2a1b7591a5790f9daf08c44619c48ba46c3a2884
Daniel Gelvez <danielgelvez.18@gmail.com>
2023-08-04 14:21:10 -0500

    Add wikipedia search

---
+++ examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/reasoning/WikipediaSearchByParamsExample.kt
@@ -0,0 +1,35 @@
+package com.xebia.functional.xef.auto.reasoning
+
+import com.xebia.functional.gpt4all.getOrThrow
+import com.xebia.functional.xef.auto.ai
+import com.xebia.functional.xef.reasoning.wikipedia.WikipediaClient
+
+suspend fun main() {
+    ai {
+        val client = WikipediaClient()
+
+        val searchDataByPageId = WikipediaClient.SearchDataByParam(
+            pageId = 5222
+        )
+        val answerByPageId = client.searchByPageId(searchDataByPageId)
+
+        val searchDataByTitle = WikipediaClient.SearchDataByParam(
+            title = "Departments%20of%20Colombia"
+        )
+        val answerByTitle = client.searchByTitle(searchDataByTitle)
+
+        println(
+            "\n\uD83E\uDD16 Search Information:\n\n" +
+                    "Title: ${answerByPageId.title}\n" +
+                    "PageId: ${answerByPageId.pageId}\n" +
+                    "Document: ${answerByPageId.document}\n"
+        )
+
+        println(
+            "\n\uD83E\uDD16 Search Information:\n\n" +
+                    "Title: ${answerByTitle.title}\n" +
+                    "PageId: ${answerByTitle.pageId}\n" +
+                    "Document: ${answerByTitle.document}\n"
+        )
+    }.getOrThrow()
+}
---
+++ reasoning/src/commonMain/kotlin/com/xebia/functional/xef/reasoning/wikipedia/WikipediaClient.kt
@@ -0,0 +1,88 @@
+package com.xebia.functional.xef.reasoning.wikipedia
+
+import com.xebia.functional.xef.auto.AutoClose
+import com.xebia.functional.xef.auto.autoClose
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+class WikipediaClient : AutoCloseable, AutoClose by autoClose() {
+
+    private val http: HttpClient = HttpClient {
+        install(HttpTimeout) {
+            requestTimeoutMillis = 60_000
+            connectTimeoutMillis = 60_000
+        }
+        install(HttpRequestRetry) {
+            maxRetries = 5
+            retryIf { _, response -> !response.status.isSuccess() }
+            retryOnExceptionIf { _, _ -> true }
+            delayMillis { retry -> retry * 3000L }
+        }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    encodeDefaults = false
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+
+    data class SearchData(val search: String)
+    data class SearchDataByParam(val pageId: Int? = null, val title: String? = null)
+
+    @Serializable
+    data class SearchResult(@SerialName("query") val searchResults: SearchResults)
+    @Serializable
+    data class SearchResults(@SerialName("search") val searches: List<Search>)
+    @Serializable
+    data class Search(
+        val title: String,
+        @SerialName("pageid") val pageId: Int,
+        @SerialName("size") val size: Int,
+        @SerialName("wordcount") val wordCount: Int,
+        @SerialName("snippet") val document: String
+    )
+
+    @Serializable
+    data class SearchByParamResult(@SerialName("query") val searchResults: SearchByParamResults)
+    @Serializable
+    data class SearchByParamResults(val pages: Map<String, Page>)
+    @Serializable
+    data class Page(
+        @SerialName("pageid") val pageId: Int,
+        val title: String,
+        @SerialName("extract") val document: String
+    )
+
+    suspend fun search(searchData: SearchData): SearchResult {
+        return http.get("https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=${searchData.search}") {
+                contentType(ContentType.Application.Json)
+            }.body<SearchResult>()
+    }
+
+    suspend fun searchByPageId(searchData: SearchDataByParam): Page {
+        return http.get("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&explaintext&redirects=1&pageids=${searchData.pageId}") {
+            contentType(ContentType.Application.Json)
+        }.body<SearchByParamResult>().searchResults.pages.firstNotNullOf { it.value }
+    }
+
+    suspend fun searchByTitle(searchData: SearchDataByParam): Page {
+        return http.get("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&explaintext&redirects=1&titles=${searchData.title}") {
+            contentType(ContentType.Application.Json)
+        }.body<SearchByParamResult>().searchResults.pages.firstNotNullOf { it.value }
+    }
+
+    override fun close() {
+        http.close()
+    }
+}
