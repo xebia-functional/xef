c73364b1e662668760c610c34a052792799ba952
Daniel Gelvez <danielgelvez.18@gmail.com>
2023-08-04 15:22:28 -0500

    Format

--- reasoning/src/commonMain/kotlin/com/xebia/functional/xef/reasoning/wikipedia/WikipediaClient.kt
+++ reasoning/src/commonMain/kotlin/com/xebia/functional/xef/reasoning/wikipedia/WikipediaClient.kt
@@ -15,74 +15,94 @@ import kotlinx.serialization.json.Json
 
 class WikipediaClient : AutoCloseable, AutoClose by autoClose() {
 
-    private val http: HttpClient = HttpClient {
-        install(HttpTimeout) {
-            requestTimeoutMillis = 60_000
-            connectTimeoutMillis = 60_000
+  private val http: HttpClient = HttpClient {
+    install(HttpTimeout) {
+      requestTimeoutMillis = 60_000
+      connectTimeoutMillis = 60_000
+    }
+    install(HttpRequestRetry) {
+      maxRetries = 5
+      retryIf { _, response -> !response.status.isSuccess() }
+      retryOnExceptionIf { _, _ -> true }
+      delayMillis { retry -> retry * 3000L }
+    }
+    install(ContentNegotiation) {
+      json(
+        Json {
+          encodeDefaults = false
+          isLenient = true
+          ignoreUnknownKeys = true
         }
-        install(HttpRequestRetry) {
-            maxRetries = 5
-            retryIf { _, response -> !response.status.isSuccess() }
-            retryOnExceptionIf { _, _ -> true }
-            delayMillis { retry -> retry * 3000L }
-        }
-        install(ContentNegotiation) {
-            json(
-                Json {
-                    encodeDefaults = false
-                    isLenient = true
-                    ignoreUnknownKeys = true
-                }
-            )
-        }
+      )
     }
+  }
 
-    data class SearchData(val search: String)
-    data class SearchDataByParam(val pageId: Int? = null, val title: String? = null)
+  data class SearchData(val search: String)
 
-    @Serializable
-    data class SearchResult(@SerialName("query") val searchResults: SearchResults)
-    @Serializable
-    data class SearchResults(@SerialName("search") val searches: List<Search>)
-    @Serializable
-    data class Search(
-        val title: String,
-        @SerialName("pageid") val pageId: Int,
-        @SerialName("size") val size: Int,
-        @SerialName("wordcount") val wordCount: Int,
-        @SerialName("snippet") val document: String
-    )
+  data class SearchDataByParam(val pageId: Int? = null, val title: String? = null)
 
-    @Serializable
-    data class SearchByParamResult(@SerialName("query") val searchResults: SearchByParamResults)
-    @Serializable
-    data class SearchByParamResults(val pages: Map<String, Page>)
-    @Serializable
-    data class Page(
-        @SerialName("pageid") val pageId: Int,
-        val title: String,
-        @SerialName("extract") val document: String
-    )
+  @Serializable data class SearchResult(@SerialName("query") val searchResults: SearchResults)
 
-    suspend fun search(searchData: SearchData): SearchResult {
-        return http.get("https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=${searchData.search}") {
-                contentType(ContentType.Application.Json)
-            }.body<SearchResult>()
-    }
+  @Serializable data class SearchResults(@SerialName("search") val searches: List<Search>)
 
-    suspend fun searchByPageId(searchData: SearchDataByParam): Page {
-        return http.get("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&explaintext&redirects=1&pageids=${searchData.pageId}") {
-            contentType(ContentType.Application.Json)
-        }.body<SearchByParamResult>().searchResults.pages.firstNotNullOf { it.value }
-    }
+  @Serializable
+  data class Search(
+    val title: String,
+    @SerialName("pageid") val pageId: Int,
+    @SerialName("size") val size: Int,
+    @SerialName("wordcount") val wordCount: Int,
+    @SerialName("snippet") val document: String
+  )
 
-    suspend fun searchByTitle(searchData: SearchDataByParam): Page {
-        return http.get("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&explaintext&redirects=1&titles=${searchData.title?.encodeURLQueryComponent()}") {
-            contentType(ContentType.Application.Json)
-        }.body<SearchByParamResult>().searchResults.pages.firstNotNullOf { it.value }
-    }
+  @Serializable
+  data class SearchByParamResult(@SerialName("query") val searchResults: SearchByParamResults)
 
-    override fun close() {
-        http.close()
-    }
+  @Serializable data class SearchByParamResults(val pages: Map<String, Page>)
+
+  @Serializable
+  data class Page(
+    @SerialName("pageid") val pageId: Int,
+    val title: String,
+    @SerialName("extract") val document: String
+  )
+
+  suspend fun search(searchData: SearchData): SearchResult {
+    return http
+      .get(
+        "https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=${searchData.search}"
+      ) {
+        contentType(ContentType.Application.Json)
+      }
+      .body<SearchResult>()
+  }
+
+  suspend fun searchByPageId(searchData: SearchDataByParam): Page {
+    return http
+      .get(
+        "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&explaintext&redirects=1&pageids=${searchData.pageId}"
+      ) {
+        contentType(ContentType.Application.Json)
+      }
+      .body<SearchByParamResult>()
+      .searchResults
+      .pages
+      .firstNotNullOf { it.value }
+  }
+
+  suspend fun searchByTitle(searchData: SearchDataByParam): Page {
+    return http
+      .get(
+        "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&explaintext&redirects=1&titles=${searchData.title?.encodeURLQueryComponent()}"
+      ) {
+        contentType(ContentType.Application.Json)
+      }
+      .body<SearchByParamResult>()
+      .searchResults
+      .pages
+      .firstNotNullOf { it.value }
+  }
+
+  override fun close() {
+    http.close()
+  }
 }
