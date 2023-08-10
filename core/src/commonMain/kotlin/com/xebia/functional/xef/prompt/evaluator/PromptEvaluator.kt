6a4b39c7089e6dcc82c2782fd92d032cf1059577
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2023-08-10 13:58:22 +0200

    Generalizes the checks (#310)

--- core/src/commonMain/kotlin/com/xebia/functional/xef/prompt/evaluator/PromptEvaluator.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/prompt/evaluator/PromptEvaluator.kt
@@ -5,26 +5,107 @@ import com.xebia.functional.xef.auto.PromptConfiguration
 import com.xebia.functional.xef.llm.Chat
 import com.xebia.functional.xef.llm.models.chat.Message
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.MapEntrySerializer
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.doubleOrNull
 
 object PromptEvaluator {
 
   @Serializable
   data class Score(
-    val completeness: Double,
-    val accuracy: Double,
-    val efficiency: Double,
-    val robustness: Double,
-    val biasAndFairness: Double,
-    val interpretability: Double,
-    val usability: Double,
-    val safetyAndSecurity: Double,
-    val creativityAndFlexibility: Double,
-    val internationalization: Double,
-    val scalability: Double,
-    val legalAndEthicalConsiderations: Double,
-    val reasoning: String,
+    val scoreCriteria: List<ScoreCriteria>,
+    val reasoning: String
   )
+
+  @Serializable
+  data class ScoreCriteria(val name: String, val score: Double)
+
+  data class ScoreCriteriaConfig(
+      val name: String,
+      val functionTitle: String,
+      val subfunctions: List<String>,
+      val evaluation: String,
+  ) {
+      companion object {
+          val DEFAULTS: List<ScoreCriteriaConfig> = listOf(
+              ScoreCriteriaConfig(
+                  "completeness",
+                  "Completeness",
+                  listOf("Coverage", "Detail"),
+                  "thorough"
+              ),
+              ScoreCriteriaConfig(
+                  "accuracy",
+                  "Accuracy",
+                  listOf("Correctness", "Consistency"),
+                  "precise"
+              ),
+              ScoreCriteriaConfig(
+                  "efficiency",
+                  "Efficiency",
+                  listOf("ResponseTime", "ResourceUtilization"),
+                  "optimized"
+              ),
+              ScoreCriteriaConfig(
+                  "robustness",
+                  "Robustness",
+                  listOf("NoiseTolerance", "Adaptability"),
+                  "resilient"
+              ),
+              ScoreCriteriaConfig(
+                  "biasAndFairness",
+                  "BiasAndFairness",
+                  listOf("DemographicBias", "ContentBias"),
+                  "unbiased"
+              ),
+              ScoreCriteriaConfig(
+                  "interpretability",
+                  "Interpretability",
+                  listOf("Clarity", "Explainability"),
+                  "clear"
+              ),
+              ScoreCriteriaConfig(
+                  "usability",
+                  "Usability",
+                  listOf("UserExperience", "Accessibility"),
+                  "user-friendly"
+              ),
+              ScoreCriteriaConfig(
+                  "safetyAndSecurity",
+                  "SafetyAndSecurity",
+                  listOf("ContentSafety", "DataSecurity"),
+                  "safe-and-secure"
+              ),
+              ScoreCriteriaConfig(
+                  "creativityAndFlexibility",
+                  "CreativityAndFlexibility",
+                  listOf("Innovation", "Versatility"),
+                  "innovative"
+              ),
+              ScoreCriteriaConfig(
+                  "internationalization",
+                  "Internationalization",
+                  listOf("LanguageSupport", "CulturalSensitivity"),
+                  "global"
+              ),
+              ScoreCriteriaConfig(
+                  "scalability",
+                  "Scalability",
+                  listOf("VolumeHandling", "Integration"),
+                  "scalable"
+              ),
+              ScoreCriteriaConfig(
+                  "legalAndEthicalConsiderations",
+                  "LegalAndEthicalConsiderations",
+                  listOf("Compliance", "EthicalConsiderations"),
+                  "compliant"
+              ),
+          )
+      }
+  }
 
   suspend fun evaluate(
     model: Chat,
@@ -32,14 +113,19 @@ object PromptEvaluator {
     prompt: String,
     response: String,
     promptConfiguration: PromptConfiguration = PromptConfiguration.DEFAULTS,
+    scoreConfig: List<ScoreCriteriaConfig> = ScoreCriteriaConfig.DEFAULTS
   ): Score {
-    val result: List<String> =
-      model.promptMessages(
-        messages =
-          listOf(
-            Message.systemMessage {
-              // language=markdown
-              """
+
+      fun printDef(c: ScoreCriteriaConfig): String =
+          "            ${c.functionTitle}() {\n${c.subfunctions.map { "              ${it}()" }.joinToString("\n")}\n            }"
+
+      fun printCall(c: ScoreCriteriaConfig): String = "                ${c.functionTitle}():evaluation=${c.evaluation}"
+
+      fun printReturn(c: ScoreCriteriaConfig): String = "                ${c.name}: {{infer:double}},"
+
+      val message =
+          // language=markdown
+          """
           # PromptEvaluator
           
           # Roleplay as an evaluator for testing the performance of prompts over LLMs.
@@ -49,54 +135,7 @@ object PromptEvaluator {
               Prompt: String
               Response: String
             }
-            Completeness() {
-              Coverage()
-              Detail()
-            }
-            Accuracy() {
-              Correctness()
-              Consistency()
-            }
-            Efficiency() {
-              ResponseTime()
-              ResourceUtilization()
-            }
-            Robustness() {
-              NoiseTolerance()
-              Adaptability()
-            }
-            BiasAndFairness() {
-              DemographicBias()
-              ContentBias()
-            }
-            Interpretability() {
-              Clarity()
-              Explainability()
-            }
-            Usability() {
-              UserExperience()
-              Accessibility()
-            }
-            SafetyAndSecurity() {
-              ContentSafety()
-              DataSecurity()
-            }
-            CreativityAndFlexibility() {
-              Innovation()
-              Versatility()
-            }
-            Internationalization() {
-              LanguageSupport()
-              CulturalSensitivity()
-            }
-            Scalability() {
-              VolumeHandling()
-              Integration()
-            }
-            LegalAndEthicalConsiderations() {
-              Compliance()
-              EthicalConsiderations()
-            }
+${scoreConfig.joinToString("\n") { printDef(it) }}
             ScoreRules() {
               Apply a score to each evaluation.
               0 = not met
@@ -109,33 +148,10 @@ object PromptEvaluator {
               Set Prompt = prompt
               Set Response = response
               ScoreRules() {
-                Completeness():evaluation=thorough
-                Accuracy():evaluation=precise
-                Efficiency():evaluation=optimized
-                Robustness():evaluation=resilient
-                BiasAndFairness():evaluation=unbiased
-                Interpretability():evaluation=clear
-                Usability():evaluation=user-friendly
-                Safety():evaluation=safe
-                Security():evaluation=secure
-                Creativity():evaluation=innovative
-                Internationalization():evaluation=global
-                Scalability():evaluation=scalable
-                LegalAndEthicalConsiderations():evaluation=compliant
+${scoreConfig.joinToString("\n") { printCall(it) }}
               }
               return {
-                completeness: {{infer:double}},
-                accuracy: {{infer:double}},
-                efficiency: {{infer:double}},
-                robustness: {{infer:double}},
-                biasAndFairness: {{infer:double}},
-                interpretability: {{infer:double}},
-                usability: {{infer:double}},
-                safetyAndSecurity: {{infer:double}},
-                creativityAndFlexibility: {{infer:double}},
-                internationalization: {{infer:double}},
-                scalability: {{infer:double}},
-                legalAndEthicalConsiderations: {{infer:double}},
+${scoreConfig.joinToString("\n") { printReturn(it) }}
                 reasoning: {{infer:string;short_answer;max_tokens=50}}
               }
             }:evaluation=complete,returns=json,stringformat=json_escaped
@@ -145,8 +161,13 @@ object PromptEvaluator {
           instructions above and ensure that the evaluation is complete in all aspects. 📝
           Reply exclusively with the evaluation JSON object. 📤
           """
-                .trimIndent()
-            },
+              .trimIndent()
+
+    val result: List<String> =
+      model.promptMessages(
+        messages =
+          listOf(
+            Message.systemMessage { message },
             Message.userMessage { "Set Prompt = $prompt" },
             Message.userMessage { "Set Response = $response" },
             Message.userMessage { "Evaluate(Prompt, Response)" }
@@ -156,6 +177,12 @@ object PromptEvaluator {
         promptConfiguration = promptConfiguration,
       )
     val firstMessage = result.firstOrNull() ?: error("No messages returned from prompt")
-    return Json.decodeFromString(Score.serializer(), firstMessage)
+    val map = Json.decodeFromString(MapSerializer(String.serializer(), JsonPrimitive.serializer()), firstMessage)
+    val values: List<ScoreCriteria> = map.toList().flatMap {
+        val value: Double? = it.second.doubleOrNull
+        if (value != null) listOf(ScoreCriteria(it.first, value)) else listOf()
+    }
+    val reasoning = map["reasoning"]?.toString()
+    return Score(values, reasoning.orEmpty())
   }
 }
