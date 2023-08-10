2fab2643b59ba46cef3d45630495daba6637c4d6
Javi Pacheco <javi.pacheco@gmail.com>
2023-08-10 15:29:32 +0200

    Testing models (#309)
    * Testing models
    
    * Comment fixed
    
    * ConversationSpec
    
    ---------
    
    Co-authored-by: Raúl Raja Martínez <raulraja@gmail.com>

---
+++ core/src/commonTest/kotlin/com/xebia/functional/xef/data/TestModel.kt
@@ -0,0 +1,59 @@
+package com.xebia.functional.xef.data
+
+import com.xebia.functional.tokenizer.ModelType
+import com.xebia.functional.xef.llm.Chat
+import com.xebia.functional.xef.llm.Embeddings
+import com.xebia.functional.xef.llm.models.chat.*
+import com.xebia.functional.xef.llm.models.embeddings.EmbeddingRequest
+import com.xebia.functional.xef.llm.models.embeddings.EmbeddingResult
+import com.xebia.functional.xef.llm.models.usage.Usage
+import kotlinx.coroutines.flow.Flow
+
+class TestModel(
+  override val modelType: ModelType,
+  override val name: String,
+  val responses: Map<String, String> = emptyMap(),
+) : Chat, Embeddings, AutoCloseable {
+
+  var requests: MutableList<ChatCompletionRequest> = mutableListOf()
+
+  override suspend fun createChatCompletion(
+    request: ChatCompletionRequest
+  ): ChatCompletionResponse {
+    requests.add(request)
+    return ChatCompletionResponse(
+      id = "fake-id",
+      `object` = "fake-object",
+      created = 0,
+      model = "fake-model",
+      choices =
+        listOf(
+          Choice(
+            message =
+              Message(
+                role = Role.USER,
+                content = responses[request.messages.last().content] ?: "fake-content",
+                name = Role.USER.name
+              ),
+            finishReason = "fake-finish-reason",
+            index = 0
+          )
+        ),
+      usage = Usage.ZERO
+    )
+  }
+
+  override suspend fun createChatCompletions(
+    request: ChatCompletionRequest
+  ): Flow<ChatCompletionChunk> {
+    throw NotImplementedError()
+  }
+
+  override fun tokensFromMessages(messages: List<Message>): Int {
+    return messages.sumOf { it.content.length }
+  }
+
+  override suspend fun createEmbeddings(request: EmbeddingRequest): EmbeddingResult {
+    return EmbeddingResult(data = emptyList(), usage = Usage.ZERO)
+  }
+}
