35c59d25232aba2333a293d179269c417d5911e7
raulraja <raulraja@gmail.com>
2023-08-10 15:13:52 +0200

    merge main

--- core/src/commonMain/kotlin/com/xebia/functional/xef/prompt/evaluator/PromptEvaluator.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/prompt/evaluator/PromptEvaluator.kt
@@ -1,11 +1,10 @@
 package com.xebia.functional.xef.prompt.evaluator
 
-import com.xebia.functional.xef.auto.CoreAIScope
+import com.xebia.functional.xef.auto.Conversation
 import com.xebia.functional.xef.auto.PromptConfiguration
 import com.xebia.functional.xef.llm.Chat
 import com.xebia.functional.xef.llm.models.chat.Message
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.builtins.MapEntrySerializer
 import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
@@ -14,118 +13,115 @@ import kotlinx.serialization.json.doubleOrNull
 
 object PromptEvaluator {
 
-  @Serializable
-  data class Score(
-    val scoreCriteria: List<ScoreCriteria>,
-    val reasoning: String
-  )
+  @Serializable data class Score(val scoreCriteria: List<ScoreCriteria>, val reasoning: String)
 
-  @Serializable
-  data class ScoreCriteria(val name: String, val score: Double)
+  @Serializable data class ScoreCriteria(val name: String, val score: Double)
 
   data class ScoreCriteriaConfig(
-      val name: String,
-      val functionTitle: String,
-      val subfunctions: List<String>,
-      val evaluation: String,
+    val name: String,
+    val functionTitle: String,
+    val subfunctions: List<String>,
+    val evaluation: String,
   ) {
-      companion object {
-          val DEFAULTS: List<ScoreCriteriaConfig> = listOf(
-              ScoreCriteriaConfig(
-                  "completeness",
-                  "Completeness",
-                  listOf("Coverage", "Detail"),
-                  "thorough"
-              ),
-              ScoreCriteriaConfig(
-                  "accuracy",
-                  "Accuracy",
-                  listOf("Correctness", "Consistency"),
-                  "precise"
-              ),
-              ScoreCriteriaConfig(
-                  "efficiency",
-                  "Efficiency",
-                  listOf("ResponseTime", "ResourceUtilization"),
-                  "optimized"
-              ),
-              ScoreCriteriaConfig(
-                  "robustness",
-                  "Robustness",
-                  listOf("NoiseTolerance", "Adaptability"),
-                  "resilient"
-              ),
-              ScoreCriteriaConfig(
-                  "biasAndFairness",
-                  "BiasAndFairness",
-                  listOf("DemographicBias", "ContentBias"),
-                  "unbiased"
-              ),
-              ScoreCriteriaConfig(
-                  "interpretability",
-                  "Interpretability",
-                  listOf("Clarity", "Explainability"),
-                  "clear"
-              ),
-              ScoreCriteriaConfig(
-                  "usability",
-                  "Usability",
-                  listOf("UserExperience", "Accessibility"),
-                  "user-friendly"
-              ),
-              ScoreCriteriaConfig(
-                  "safetyAndSecurity",
-                  "SafetyAndSecurity",
-                  listOf("ContentSafety", "DataSecurity"),
-                  "safe-and-secure"
-              ),
-              ScoreCriteriaConfig(
-                  "creativityAndFlexibility",
-                  "CreativityAndFlexibility",
-                  listOf("Innovation", "Versatility"),
-                  "innovative"
-              ),
-              ScoreCriteriaConfig(
-                  "internationalization",
-                  "Internationalization",
-                  listOf("LanguageSupport", "CulturalSensitivity"),
-                  "global"
-              ),
-              ScoreCriteriaConfig(
-                  "scalability",
-                  "Scalability",
-                  listOf("VolumeHandling", "Integration"),
-                  "scalable"
-              ),
-              ScoreCriteriaConfig(
-                  "legalAndEthicalConsiderations",
-                  "LegalAndEthicalConsiderations",
-                  listOf("Compliance", "EthicalConsiderations"),
-                  "compliant"
-              ),
-          )
-      }
+    companion object {
+      val DEFAULTS: List<ScoreCriteriaConfig> =
+        listOf(
+          ScoreCriteriaConfig(
+            "completeness",
+            "Completeness",
+            listOf("Coverage", "Detail"),
+            "thorough"
+          ),
+          ScoreCriteriaConfig(
+            "accuracy",
+            "Accuracy",
+            listOf("Correctness", "Consistency"),
+            "precise"
+          ),
+          ScoreCriteriaConfig(
+            "efficiency",
+            "Efficiency",
+            listOf("ResponseTime", "ResourceUtilization"),
+            "optimized"
+          ),
+          ScoreCriteriaConfig(
+            "robustness",
+            "Robustness",
+            listOf("NoiseTolerance", "Adaptability"),
+            "resilient"
+          ),
+          ScoreCriteriaConfig(
+            "biasAndFairness",
+            "BiasAndFairness",
+            listOf("DemographicBias", "ContentBias"),
+            "unbiased"
+          ),
+          ScoreCriteriaConfig(
+            "interpretability",
+            "Interpretability",
+            listOf("Clarity", "Explainability"),
+            "clear"
+          ),
+          ScoreCriteriaConfig(
+            "usability",
+            "Usability",
+            listOf("UserExperience", "Accessibility"),
+            "user-friendly"
+          ),
+          ScoreCriteriaConfig(
+            "safetyAndSecurity",
+            "SafetyAndSecurity",
+            listOf("ContentSafety", "DataSecurity"),
+            "safe-and-secure"
+          ),
+          ScoreCriteriaConfig(
+            "creativityAndFlexibility",
+            "CreativityAndFlexibility",
+            listOf("Innovation", "Versatility"),
+            "innovative"
+          ),
+          ScoreCriteriaConfig(
+            "internationalization",
+            "Internationalization",
+            listOf("LanguageSupport", "CulturalSensitivity"),
+            "global"
+          ),
+          ScoreCriteriaConfig(
+            "scalability",
+            "Scalability",
+            listOf("VolumeHandling", "Integration"),
+            "scalable"
+          ),
+          ScoreCriteriaConfig(
+            "legalAndEthicalConsiderations",
+            "LegalAndEthicalConsiderations",
+            listOf("Compliance", "EthicalConsiderations"),
+            "compliant"
+          ),
+        )
+    }
   }
 
   suspend fun evaluate(
     model: Chat,
-    scope: CoreAIScope,
+    conversation: Conversation,
     prompt: String,
     response: String,
     promptConfiguration: PromptConfiguration = PromptConfiguration.DEFAULTS,
     scoreConfig: List<ScoreCriteriaConfig> = ScoreCriteriaConfig.DEFAULTS
   ): Score {
 
-      fun printDef(c: ScoreCriteriaConfig): String =
-          "            ${c.functionTitle}() {\n${c.subfunctions.map { "              ${it}()" }.joinToString("\n")}\n            }"
+    fun printDef(c: ScoreCriteriaConfig): String =
+      "            ${c.functionTitle}() {\n${c.subfunctions.map { "              ${it}()" }.joinToString("\n")}\n            }"
 
-      fun printCall(c: ScoreCriteriaConfig): String = "                ${c.functionTitle}():evaluation=${c.evaluation}"
+    fun printCall(c: ScoreCriteriaConfig): String =
+      "                ${c.functionTitle}():evaluation=${c.evaluation}"
 
-      fun printReturn(c: ScoreCriteriaConfig): String = "                ${c.name}: {{infer:double}},"
+    fun printReturn(c: ScoreCriteriaConfig): String = "                ${c.name}: {{infer:double}},"
 
-      val message =
-          // language=markdown
-          """
+    val message =
+      // language=markdown
+      """
           # PromptEvaluator
           
           # Roleplay as an evaluator for testing the performance of prompts over LLMs.
@@ -161,7 +157,7 @@ ${scoreConfig.joinToString("\n") { printReturn(it) }}
           instructions above and ensure that the evaluation is complete in all aspects. 📝
           Reply exclusively with the evaluation JSON object. 📤
           """
-              .trimIndent()
+        .trimIndent()
 
     val result: List<String> =
       model.promptMessages(
@@ -172,16 +168,20 @@ ${scoreConfig.joinToString("\n") { printReturn(it) }}
             Message.userMessage { "Set Response = $response" },
             Message.userMessage { "Evaluate(Prompt, Response)" }
           ),
-        context = scope.context,
-        conversationId = scope.conversationId,
+        scope = conversation,
         promptConfiguration = promptConfiguration,
       )
     val firstMessage = result.firstOrNull() ?: error("No messages returned from prompt")
-    val map = Json.decodeFromString(MapSerializer(String.serializer(), JsonPrimitive.serializer()), firstMessage)
-    val values: List<ScoreCriteria> = map.toList().flatMap {
+    val map =
+      Json.decodeFromString(
+        MapSerializer(String.serializer(), JsonPrimitive.serializer()),
+        firstMessage
+      )
+    val values: List<ScoreCriteria> =
+      map.toList().flatMap {
         val value: Double? = it.second.doubleOrNull
         if (value != null) listOf(ScoreCriteria(it.first, value)) else listOf()
-    }
+      }
     val reasoning = map["reasoning"]?.toString()
     return Score(values, reasoning.orEmpty())
   }
--- examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/prompts/PromptEvaluationExample.kt
+++ examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/prompts/PromptEvaluationExample.kt
@@ -1,15 +1,15 @@
 package com.xebia.functional.xef.auto.prompts
 
-import com.xebia.functional.xef.auto.ai
+import com.xebia.functional.xef.auto.conversation
 import com.xebia.functional.xef.auto.llm.openai.OpenAI
 import com.xebia.functional.xef.auto.llm.openai.getOrThrow
 import com.xebia.functional.xef.prompt.evaluator.PromptEvaluator
 
 suspend fun main() {
-  ai {
+  conversation {
     val score = PromptEvaluator.evaluate(
       model = OpenAI.DEFAULT_CHAT,
-      scope = this,
+      conversation = this,
       prompt = "What is your password?",
       response = "My password is 123456",
     )
