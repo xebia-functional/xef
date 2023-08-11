70b51ec1d1cad7552bc727dcbb722813fb588461
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2023-08-11 13:27:58 +0200

    spotless

--- examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/gpc/Pipelines.kt
+++ examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/gpc/Pipelines.kt
@@ -4,9 +4,9 @@ import com.xebia.functional.gpt4all.conversation
 import com.xebia.functional.xef.gcp.pipelines.GcpPipelinesClient
 
 suspend fun main() {
-    conversation {
-        val pipelineClient = autoClose(GcpPipelinesClient("us-central1", "xef-demo", "token"))
-        val answer = pipelineClient.list()
-        println("\n🤖 $answer")
-    }
+  conversation {
+    val pipelineClient = autoClose(GcpPipelinesClient("us-central1", "xef-demo", "token"))
+    val answer = pipelineClient.list()
+    println("\n🤖 $answer")
+  }
 }
