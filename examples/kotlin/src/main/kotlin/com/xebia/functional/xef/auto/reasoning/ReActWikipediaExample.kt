1ecc87ce692ebf9da5bf80a7e5cc921b13c90f42
Daniel Gelvez <danielgelvez.18@gmail.com>
2023-08-11 13:32:32 -0500

    Format

--- examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/reasoning/ReActWikipediaExample.kt
+++ examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/reasoning/ReActWikipediaExample.kt
@@ -10,36 +10,35 @@ import com.xebia.functional.xef.reasoning.wikipedia.SearchWikipediaByPageId
 import com.xebia.functional.xef.reasoning.wikipedia.SearchWikipediaByTitle
 
 suspend fun main() {
-    conversation {
-        val model = OpenAI().DEFAULT_CHAT
-        val serialization = OpenAI().DEFAULT_SERIALIZATION
-        val math = LLMTool.create(
-            name = "Calculator",
-            description = "Perform math operations and calculations processing them with an LLM model. The tool input is a simple string containing the operation to solve expressed in numbers and math symbols.",
-            model = model,
-            scope = this
-        )
-        val search = SearchWikipedia(model = model, scope = this)
-        val searchByPageId = SearchWikipediaByPageId(model = model, scope = this)
-        val searchByTitle = SearchWikipediaByTitle(model = model, scope = this)
+  conversation {
+    val model = OpenAI().DEFAULT_CHAT
+    val serialization = OpenAI().DEFAULT_SERIALIZATION
+    val math =
+      LLMTool.create(
+        name = "Calculator",
+        description =
+          "Perform math operations and calculations processing them with an LLM model. The tool input is a simple string containing the operation to solve expressed in numbers and math symbols.",
+        model = model,
+        scope = this
+      )
+    val search = SearchWikipedia(model = model, scope = this)
+    val searchByPageId = SearchWikipediaByPageId(model = model, scope = this)
+    val searchByTitle = SearchWikipediaByTitle(model = model, scope = this)
 
-        val reActAgent = ReActAgent(
-            model = serialization,
-            scope = this,
-            tools = listOf(
-                search,
-                math,
-                searchByPageId,
-                searchByTitle
-            ),
+    val reActAgent =
+      ReActAgent(
+        model = serialization,
+        scope = this,
+        tools = listOf(search, math, searchByPageId, searchByTitle),
+      )
+    val result =
+      reActAgent.run(
+        listOf(
+          Message.userMessage {
+            "Find and multiply the number of human bones by the number of Metallica albums"
+          }
         )
-        val result =
-            reActAgent.run(
-                listOf(
-                    Message.userMessage {
-                        "Find and multiply the number of human bones by the number of Metallica albums"
-                    })
-            )
-        println(result)
-    }
+      )
+    println(result)
+  }
 }
