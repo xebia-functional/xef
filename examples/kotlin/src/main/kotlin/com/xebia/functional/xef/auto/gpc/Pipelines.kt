c14f5717ee478b31c114143ee5ff3c1c32acf43e
Fede Fernández <720923+fedefernandez@users.noreply.github.com>
2023-08-11 13:00:32 +0200

    Updates to conversational

--- examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/gpc/Pipelines.kt
+++ examples/kotlin/src/main/kotlin/com/xebia/functional/xef/auto/gpc/Pipelines.kt
@@ -1,13 +1,12 @@
 package com.xebia.functional.xef.auto.gpc
 
-import com.xebia.functional.gpt4all.getOrThrow
-import com.xebia.functional.xef.auto.ai
+import com.xebia.functional.gpt4all.conversation
 import com.xebia.functional.xef.gcp.pipelines.GcpPipelinesClient
 
 suspend fun main() {
-    ai {
+    conversation {
         val pipelineClient = autoClose(GcpPipelinesClient("us-central1", "xef-demo", "token"))
         val answer = pipelineClient.list()
         println("\n🤖 $answer")
-    }.getOrThrow()
+    }
 }
