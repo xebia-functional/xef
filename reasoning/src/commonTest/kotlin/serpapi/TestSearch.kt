c32d384fb4f2ac2b932d8f7faa56b0f951312347
Daniel Gelvez <danielgelvez.18@gmail.com>
2023-08-29 17:32:32 -0500

    Test wikipedia by title and by pageid

--- reasoning/src/commonTest/kotlin/serpapi/TestSearch.kt
+++ reasoning/src/commonTest/kotlin/serpapi/TestSearch.kt
@@ -2,16 +2,12 @@ package serpapi
 
 import com.xebia.functional.xef.conversation.Conversation
 import com.xebia.functional.xef.llm.Chat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 
 class TestSearch(
   override val model: Chat,
   override val scope: Conversation,
   override val maxResultsInContext: Int = 3,
 ) : TestSearchTool, AutoCloseable {
-
-  private val coroutineScope = CoroutineScope(SupervisorJob())
 
   override val client = TestSerpApiClient()
 
