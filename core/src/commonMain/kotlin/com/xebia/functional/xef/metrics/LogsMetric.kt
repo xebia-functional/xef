022efc25a98545d0e7d53dded233034acbdbc9ae
José Carlos Montañez <josecarlos.aragon@47deg.com>
2023-10-26 15:57:54 +0200

    removed logger

--- core/src/commonMain/kotlin/com/xebia/functional/xef/metrics/LogsMetric.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/metrics/LogsMetric.kt
@@ -2,11 +2,9 @@ package com.xebia.functional.xef.metrics
 
 import com.xebia.functional.xef.conversation.Conversation
 import com.xebia.functional.xef.prompt.Prompt
-import io.github.oshai.kotlinlogging.KLogger
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.util.date.*
 
-class LogsMetric(val logger: KLogger = KotlinLogging.logger {}) : Metric {
+class LogsMetric : Metric {
 
   private val indentSize = 4
 
@@ -17,17 +15,15 @@ class LogsMetric(val logger: KLogger = KotlinLogging.logger {}) : Metric {
   ): A {
     val millis = getTimeMillis()
     val name = prompt.messages.lastOrNull()?.content ?: "empty"
-    logger.info { "Prompt-Span: $name" }
-    logger.info {
-      "${writeIndent()}|-- Conversation Id: ${conversation.conversationId?.value ?: "empty"}"
-    }
+    println("Prompt-Span: $name")
+    println("${writeIndent()}|-- Conversation Id: ${conversation.conversationId?.value ?: "empty"}")
     val output = block()
-    logger.info { "${writeIndent()}|-- Finished in ${getTimeMillis()-millis} ms" }
+    println("${writeIndent()}|-- Finished in ${getTimeMillis() - millis} ms")
     return output
   }
 
   override fun log(conversation: Conversation, message: String) {
-    logger.info { "${writeIndent()}|-- $message" }
+    println("${writeIndent()}|-- $message")
   }
 
   private fun writeIndent(times: Int = 1) = (1..indentSize * times).fold("") { a, b -> "$a " }
