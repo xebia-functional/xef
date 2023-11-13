439daf15b2f4cd67c93ea0c932e41a7a51d08497
raulraja <raulraja@gmail.com>
2023-11-13 23:33:43 +0100

    progress until streaming problem adapting Chat to ChatApi

--- core/src/commonMain/kotlin/com/xebia/functional/xef/llm/Chat.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/llm/Chat.kt
@@ -1,5 +1,6 @@
 package com.xebia.functional.xef.llm
 
+import com.xebia.functional.openai.apis.ChatApi
 import com.xebia.functional.openai.models.CreateChatCompletionRequest
 import com.xebia.functional.openai.models.CreateChatCompletionResponse
 import com.xebia.functional.openai.models.CreateChatCompletionStreamResponse
@@ -10,80 +11,65 @@ import com.xebia.functional.xef.prompt.Prompt
 import com.xebia.functional.xef.prompt.templates.assistant
 import kotlinx.coroutines.flow.*
 
-interface Chat : LLM {
 
-  suspend fun createChatCompletion(
-    request: CreateChatCompletionRequest
-  ): CreateChatCompletionResponse
+@AiDsl
+fun ChatApi.promptStreaming(prompt: Prompt, scope: Conversation): Flow<String> = flow {
+  val messagesForRequestPrompt =
+    PromptCalculator.adaptPromptToConversationAndModel(prompt, scope, this)
 
-  suspend fun createChatCompletions(
-    request: CreateChatCompletionRequest
-  ): Flow<CreateChatCompletionStreamResponse>
+  val request =
+    CreateChatCompletionRequest(
+      user = prompt.configuration.user,
+      messages = messagesForRequestPrompt.messages,
+      n = prompt.configuration.numberOfPredictions,
+      temperature = prompt.configuration.temperature,
+      maxTokens = prompt.configuration.minResponseTokens,
+      model = modelType.toRequestModel()
+    )
 
-  @AiDsl
-  fun promptStreaming(prompt: Prompt, scope: Conversation): Flow<String> = flow {
-    val messagesForRequestPrompt =
+  this@promptStreaming.createChatCompletion()createChatCompletions(request)
+    .mapNotNull { it.choices.mapNotNull { it.delta?.content }.reduceOrNull(String::plus) }
+    .onEach { emit(it) }
+    .fold("", String::plus)
+    .also { finalText ->
+      val aiResponseMessage = assistant(finalText)
+      val newMessages = prompt.messages + listOf(aiResponseMessage)
+      newMessages.addToMemory(scope, prompt.configuration.messagePolicy.addMessagesToConversation)
+    }
+}
+
+@AiDsl
+suspend fun promptMessage(prompt: Prompt, scope: Conversation): String =
+  promptMessages(prompt, scope).firstOrNull() ?: throw AIError.NoResponse()
+
+@AiDsl
+suspend fun promptMessages(prompt: Prompt, scope: Conversation): List<String> =
+  scope.metric.promptSpan(prompt) {
+    val promptMemories = prompt.messages.toMemory(scope)
+    val adaptedPrompt =
       PromptCalculator.adaptPromptToConversationAndModel(prompt, scope, this@Chat)
+
+    adaptedPrompt.addMetrics(scope)
 
     val request =
       CreateChatCompletionRequest(
-        user = prompt.configuration.user,
-        messages = messagesForRequestPrompt.messages,
-        n = prompt.configuration.numberOfPredictions,
-        temperature = prompt.configuration.temperature,
-        maxTokens = prompt.configuration.minResponseTokens,
+        user = adaptedPrompt.configuration.user,
+        messages = adaptedPrompt.messages,
+        n = adaptedPrompt.configuration.numberOfPredictions,
+        temperature = adaptedPrompt.configuration.temperature,
+        maxTokens = adaptedPrompt.configuration.minResponseTokens,
         model = modelType.toRequestModel()
       )
 
-    createChatCompletions(request)
-      .mapNotNull { it.choices.mapNotNull { it.delta?.content }.reduceOrNull(String::plus) }
-      .onEach { emit(it) }
-      .fold("", String::plus)
-      .also { finalText ->
-        val aiResponseMessage = assistant(finalText)
-        val newMessages = prompt.messages + listOf(aiResponseMessage)
-        newMessages.addToMemory(scope, prompt.configuration.messagePolicy.addMessagesToConversation)
-      }
+    createChatCompletion(request)
+      .addMetrics(scope)
+      .choices
+      .addChoiceToMemory(
+        scope,
+        promptMemories,
+        prompt.configuration.messagePolicy.addMessagesToConversation
+      )
+      .mapNotNull { it.message?.content }
   }
 
-  @AiDsl
-  suspend fun promptMessage(prompt: Prompt, scope: Conversation): String =
-    promptMessages(prompt, scope).firstOrNull() ?: throw AIError.NoResponse()
 
-  @AiDsl
-  suspend fun promptMessages(prompt: Prompt, scope: Conversation): List<String> =
-    scope.metric.promptSpan(prompt) {
-      val promptMemories = prompt.messages.toMemory(scope)
-      val adaptedPrompt =
-        PromptCalculator.adaptPromptToConversationAndModel(prompt, scope, this@Chat)
-
-      adaptedPrompt.addMetrics(scope)
-
-      val request =
-        CreateChatCompletionRequest(
-          user = adaptedPrompt.configuration.user,
-          messages = adaptedPrompt.messages,
-          n = adaptedPrompt.configuration.numberOfPredictions,
-          temperature = adaptedPrompt.configuration.temperature,
-          maxTokens = adaptedPrompt.configuration.minResponseTokens,
-          model = modelType.toRequestModel()
-        )
-
-      createChatCompletion(request)
-        .addMetrics(scope)
-        .choices
-        .addChoiceToMemory(
-          scope,
-          promptMemories,
-          prompt.configuration.messagePolicy.addMessagesToConversation
-        )
-        .mapNotNull { it.message?.content }
-    }
-
-  companion object {
-
-
-
-    fun fromEnvironment(): Chat = ChatImpl()
-  }
-}
