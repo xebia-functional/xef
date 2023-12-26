b499a6e636591f87f04e8e118415fd312c517984
franciscodr <francisco.diaz@xebia.com>
2023-12-26 15:45:59 +0100

    Rename span's attributes

--- core/src/commonMain/kotlin/com/xebia/functional/xef/llm/MetricManagement.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/llm/MetricManagement.kt
@@ -7,24 +7,24 @@ import com.xebia.functional.xef.prompt.Prompt
 suspend fun CreateChatCompletionResponse.addMetrics(
   conversation: Conversation
 ): CreateChatCompletionResponse {
-  conversation.metric.parameter("model", model)
+  conversation.metric.parameter("openai.chat_completion.model", model)
   usage?.let {
-    conversation.metric.parameter("tokens.prompt", "${it.promptTokens}")
-    conversation.metric.parameter("tokens.completion", "${it.completionTokens}")
-    conversation.metric.parameter("tokens.total", "${it.totalTokens}")
+    conversation.metric.parameter("openai.chat_completion.prompt.token.count", "${it.promptTokens}")
+    conversation.metric.parameter("openai.chat_completion.completion.token.count", "${it.completionTokens}")
+    conversation.metric.parameter("openai.chat_completion.token.count", "${it.totalTokens}")
   }
   return this
 }
 
 suspend fun <T> Prompt<T>.addMetrics(conversation: Conversation) {
   conversation.metric.parameter(
-    "number-of-messages",
+    "openai.chat_completion.prompt.message.count",
     "${messages.size} (${messages.map { it.completionRole().value.firstOrNull() ?: "" }.joinToString("-")})"
   )
-  conversation.metric.parameter("conversation-id", conversation.conversationId?.value ?: "none")
+  conversation.metric.parameter("openai.chat_completion.conversation.id", conversation.conversationId?.value ?: "none")
   conversation.metric.parameter(
     "functions",
     if (functions.isEmpty()) "no" else functions.joinToString(",") { it.name }
   )
-  conversation.metric.parameter("temperature", "${configuration.temperature}")
+  conversation.metric.parameter("openai.chat_completion.prompt.temperature", "${configuration.temperature}")
 }
