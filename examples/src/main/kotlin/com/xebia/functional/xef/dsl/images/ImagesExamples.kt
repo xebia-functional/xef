4ed26edb33536d09646b862a3d683251df423578
raulraja <raulraja@gmail.com>
2023-12-29 10:35:07 +0100

    fix example

--- examples/src/main/kotlin/com/xebia/functional/xef/dsl/images/ImagesExamples.kt
+++ examples/src/main/kotlin/com/xebia/functional/xef/dsl/images/ImagesExamples.kt
@@ -4,10 +4,10 @@ import com.xebia.functional.xef.AI
 
 suspend fun main() {
   val ai = AI.images()
-  val marsImages = ai.image(prompt = "Event horizon in a black hole", amount = 1)
-  marsImages.collect { image ->
+  val images = ai.image(prompt = "Event horizon in a black hole", amount = 1)
+  images.collect { image ->
     println("Image: $image")
-    val marsVariants = ai.variant(image)
-    marsVariants.collect { variant -> println("Variant: $variant") }
+    val variants = ai.variant(image)
+    variants.collect { variant -> println("Variant: $variant") }
   }
 }
