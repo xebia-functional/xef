34ad9d26aae47eb77c004edb3b592f97e43b1d5d
Raúl Raja Martínez <raulraja@gmail.com>
2024-03-05 14:02:38 +0100

    Assistants YAML config (#643)
    * Do not attempt to call functions without arguments
    
    * Creates or updates an Assistant based on YAML config
    
    * Use https://github.com/Him188/yamlkt to hand parse in kmp
    
    * removed println
    
    * Apply spotless formatting
    
    * Update core/src/commonMain/kotlin/com/xebia/functional/xef/llm/assistants/Assistant.kt
    
    Co-authored-by: David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
    
    * Update core/src/commonMain/kotlin/com/xebia/functional/xef/llm/assistants/Assistant.kt
    
    Co-authored-by: David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
    
    * Apply spotless formatting
    
    * fix fqn
    
    * example
    
    * YAML is able to load tools from the tool config
    
    * use simple name for kmp support
    
    * review comments
    
    ---------
    
    Co-authored-by: raulraja <raulraja@users.noreply.github.com>
    Co-authored-by: David Vega <7826728+realdavidvega@users.noreply.github.com>

--- core/src/commonMain/kotlin/com/xebia/functional/xef/llm/assistants/Assistant.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/llm/assistants/Assistant.kt
@@ -5,14 +5,21 @@ import com.xebia.functional.openai.apis.AssistantsApi
 import com.xebia.functional.openai.infrastructure.ApiClient
 import com.xebia.functional.openai.models.AssistantObject
 import com.xebia.functional.openai.models.CreateAssistantRequest
+import com.xebia.functional.openai.models.FunctionObject
 import com.xebia.functional.openai.models.ModifyAssistantRequest
 import com.xebia.functional.openai.models.ext.assistant.AssistantTools
+import com.xebia.functional.openai.models.ext.assistant.AssistantToolsCode
+import com.xebia.functional.openai.models.ext.assistant.AssistantToolsFunction
+import com.xebia.functional.openai.models.ext.assistant.AssistantToolsRetrieval
 import com.xebia.functional.xef.llm.fromEnvironment
 import io.ktor.util.logging.*
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
+import net.mamoe.yamlkt.Yaml
+import net.mamoe.yamlkt.literalContentOrNull
+import net.mamoe.yamlkt.toYamlElement
 
 class Assistant(
   val assistantId: String,
@@ -97,5 +104,117 @@ class Assistant(
       val response = assistantsApi.createAssistant(request)
       return Assistant(response.body(), toolsConfig, assistantsApi, api)
     }
+
+    suspend fun fromConfig(
+      request: String,
+      toolsConfig: List<Tool.Companion.ToolConfig<*, *>> = emptyList(),
+      assistantsApi: AssistantsApi = fromEnvironment(::AssistantsApi),
+      api: AssistantApi = fromEnvironment(::AssistantApi)
+    ): Assistant {
+      val parsed = Yaml.Default.decodeYamlMapFromString(request)
+      val assistantRequest =
+        AssistantRequest(
+          assistantId = parsed["assistant_id"]?.literalContentOrNull,
+          model = parsed["model"]?.literalContentOrNull ?: error("model is required"),
+          name = parsed["name"]?.literalContentOrNull,
+          description = parsed["description"]?.literalContentOrNull,
+          instructions = parsed["instructions"]?.literalContentOrNull,
+          tools =
+            parsed["tools"]?.let { list ->
+              (list as List<*>).map { element ->
+                when (element) {
+                  is Map<*, *> -> {
+                    val tool =
+                      element["type".toYamlElement()]?.toString() ?: error("type is required")
+                    when (tool) {
+                      "code_interpreter" -> AssistantTool.CodeInterpreter
+                      "retrieval" -> AssistantTool.Retrieval
+                      "function" -> {
+                        val className =
+                          element["name".toYamlElement()]?.toString()
+                            ?: error("simple `name` for `function` is required")
+                        val foundConfig =
+                          toolsConfig.firstOrNull { it.tool::class.simpleName == className }
+                        if (foundConfig != null) {
+                          val functionObject = foundConfig.functionObject
+                          AssistantTool.Function(
+                            functionObject.name,
+                            functionObject.description ?: "",
+                            functionObject.parameters?.let {
+                              ApiClient.JSON_DEFAULT.encodeToString(JsonObject.serializer(), it)
+                            } ?: ""
+                          )
+                        } else {
+                          error("Tool $className not found in toolsConfig")
+                        }
+                      }
+                      else -> error("unknown tool $tool")
+                    }
+                  }
+                  else -> error("unknown tool $element")
+                }
+              }
+            },
+          fileIds =
+            parsed["file_ids"]?.let { (it as List<*>).map { it.toString() } } ?: emptyList(),
+        )
+      return if (assistantRequest.assistantId != null) {
+        val assistant =
+          Assistant(
+            assistantId = assistantRequest.assistantId,
+            toolsConfig = toolsConfig,
+            assistantsApi = assistantsApi,
+            api = api
+          )
+        // list all assistants and get their files
+        // list all the org files
+        assistantsApi.listAssistants()
+
+        assistant.modify(
+          ModifyAssistantRequest(
+            name = assistantRequest.name,
+            description = assistantRequest.description,
+            instructions = assistantRequest.instructions,
+            tools = assistantTools(assistantRequest),
+            fileIds = assistantRequest.fileIds,
+            metadata = null // assistantRequest.metadata
+          )
+        )
+      } else
+        Assistant(
+          request =
+            CreateAssistantRequest(
+              model = assistantRequest.model,
+              name = assistantRequest.name,
+              description = assistantRequest.description,
+              instructions = assistantRequest.instructions,
+              tools = assistantTools(assistantRequest),
+              fileIds = assistantRequest.fileIds,
+              metadata = null // assistantRequest.metadata
+            ),
+          toolsConfig = toolsConfig,
+          assistantsApi = assistantsApi,
+          api = api
+        )
+    }
+
+    private fun assistantTools(assistantRequest: AssistantRequest) =
+      assistantRequest.tools?.map {
+        when (it) {
+          is AssistantTool.CodeInterpreter -> AssistantToolsCode()
+          is AssistantTool.Retrieval -> AssistantToolsRetrieval()
+          is AssistantTool.Function ->
+            AssistantToolsFunction(
+              function =
+                FunctionObject(
+                  name = it.name,
+                  parameters =
+                    ApiClient.JSON_DEFAULT.parseToJsonElement(it.parameters) as? JsonObject
+                      ?: JsonObject(emptyMap()),
+                  description = it.description
+                )
+            )
+        }
+      }
   }
 }
