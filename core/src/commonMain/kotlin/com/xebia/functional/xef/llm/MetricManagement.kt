cd2f923bc131779b41d948f3be1acde4720c8cc6
Javier Pérez Pacheco <javi.pacheco@gmail.com>
2024-03-05 13:45:19 +0100

    Assistant metrics (#674)
    * First approach for assistant metrics
    
    * Support for messages
    
    * Adding usage on metrics
    
    * OpenTelemetric comment

--- core/src/commonMain/kotlin/com/xebia/functional/xef/llm/MetricManagement.kt
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/llm/MetricManagement.kt
@@ -1,7 +1,9 @@
 package com.xebia.functional.xef.llm
 
 import com.xebia.functional.openai.models.CreateChatCompletionResponse
+import com.xebia.functional.openai.models.RunObject
 import com.xebia.functional.xef.conversation.Conversation
+import com.xebia.functional.xef.metrics.Metric
 import com.xebia.functional.xef.prompt.Prompt
 
 suspend fun CreateChatCompletionResponse.addMetrics(
@@ -40,4 +42,9 @@ suspend fun <T> Prompt<T>.addMetrics(conversation: Conversation) {
   )
   if (functions.isNotEmpty())
     conversation.metric.parameter("openai.chat_completion.functions", functions.map { it.name })
+}
+
+suspend fun RunObject.addMetrics(metric: Metric): RunObject {
+  metric.assistantCreateRun(this)
+  return this
 }
--- examples/src/main/kotlin/com/xebia/functional/xef/assistants/DSL.kt
+++ examples/src/main/kotlin/com/xebia/functional/xef/assistants/DSL.kt
@@ -6,7 +6,7 @@ import com.xebia.functional.openai.models.ext.assistant.RunStepDetailsToolCallsO
 import com.xebia.functional.xef.llm.assistants.Assistant
 import com.xebia.functional.xef.llm.assistants.AssistantThread
 import com.xebia.functional.xef.llm.assistants.Tool
-import io.ktor.client.*
+import com.xebia.functional.xef.metrics.Metric
 import kotlinx.serialization.Serializable
 
 @Serializable data class SumInput(val left: Int, val right: Int)
@@ -37,12 +37,21 @@ suspend fun main() {
   //    model = "gpt-4-1106-preview"
   //  )
   // println("generated assistant: ${assistant2.assistantId}")
+
+  // This example contemplate the case of using OpenTelemetry for metrics
+  // To run the example with OpenTelemetry, you can execute the following commands:
+  //  - # cd server/docker/opentelemetry
+  //  - # docker-compose up
+
+  val metric = Metric.EMPTY
+  // val metric = com.xebia.functional.xef.opentelemetry.OpenTelemetryMetric()
+
   val assistant =
     Assistant(
       assistantId = "asst_UxczzpJkysC0l424ood87DAk",
-      toolsConfig = listOf(Tool.toolOf(SumTool())),
+      toolsConfig = listOf(Tool.toolOf(SumTool()))
     )
-  val thread = AssistantThread()
+  val thread = AssistantThread(metric = metric)
   println("Welcome to the Math tutor, ask me anything about math:")
   while (true) {
     println()
@@ -127,5 +136,5 @@ private fun stepStatusEmoji(status: RunStepObject.Status) =
   }
 
 private fun displayRunStatus(run: AssistantThread.RunDelta.Run) {
-  println("Assistant: ${runStatusEmoji(run)}")
+  println("Assistant: ${runStatusEmoji(run)} - ${run.message.status}")
 }
