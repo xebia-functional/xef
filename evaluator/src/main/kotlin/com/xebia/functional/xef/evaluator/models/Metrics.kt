9078d85733b735a8ea08853c6eef10bcfacfdd83
José Carlos Montañez <josecarlos.aragon@47deg.com>
2024-03-21 17:06:09 +0100

    removed non necessary object

--- evaluator/src/main/kotlin/com/xebia/functional/xef/evaluator/models/Metrics.kt
+++
@@ -1,7 +0,0 @@
-package com.xebia.functional.xef.evaluator.models
-
-sealed interface MetricValues
-
-sealed interface Metric {
-  fun template(input: String, output: String, context: String, metricValues: MetricValues): String
-}
