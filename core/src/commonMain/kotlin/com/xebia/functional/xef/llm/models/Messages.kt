d9dfd74bce2d43e99450cba2163f3b48b6fa4735
Ana Mª Marquez <anamv84@gmail.com>
2024-05-14 11:23:04 +0200

    Updated branch with main (#739)
    * Tokens in Evaluator Tests (#730)
    
    * Estimate price in Evaluator Tests (#731)
    
    * Enum description in tools (#732)
    
    * support enum descriptions
    
    * added example
    
    ---------
    
    Co-authored-by: José Carlos Montañez <jc@MacBook-Pro.local>
    Co-authored-by: Raúl Raja Martínez <raulraja@gmail.com>
    
    * Update README with instruction to build locally (#725)
    
    * Add README instructions for building Xef
    
    * Include reasons why build may fail if you don't have docker
    
    * fixed error in enum description (#733)
    
    Co-authored-by: José Carlos Montañez <jc@MacBook-Pro.local>
    
    ---------
    
    Co-authored-by: Javier Pérez Pacheco <javi.pacheco@gmail.com>
    Co-authored-by: José Carlos Montañez <josecarlos.montanez@xebia.com>
    Co-authored-by: José Carlos Montañez <jc@MacBook-Pro.local>
    Co-authored-by: Raúl Raja Martínez <raulraja@gmail.com>

---
+++ core/src/commonMain/kotlin/com/xebia/functional/xef/llm/models/Messages.kt
@@ -0,0 +1,18 @@
+package com.xebia.functional.xef.llm.models
+
+import com.xebia.functional.openai.generated.model.CompletionUsage
+
+data class MessagesWithUsage(val messages: List<String>, val usage: MessagesUsage?)
+
+data class MessageWithUsage(val message: String, val usage: MessagesUsage?)
+
+data class MessagesUsage(val completionTokens: Int, val promptTokens: Int, val totalTokens: Int) {
+  companion object {
+    operator fun invoke(usage: CompletionUsage) =
+      MessagesUsage(
+        completionTokens = usage.completionTokens,
+        promptTokens = usage.promptTokens,
+        totalTokens = usage.totalTokens
+      )
+  }
+}
---
+++ examples/src/main/kotlin/com/xebia/functional/xef/assistants/ToolsWithDescriptions.kt
@@ -0,0 +1,36 @@
+package com.xebia.functional.xef.assistants
+
+import com.xebia.functional.xef.conversation.Description
+import com.xebia.functional.xef.llm.assistants.Assistant
+import com.xebia.functional.xef.llm.assistants.AssistantThread
+import com.xebia.functional.xef.llm.assistants.RunDelta
+import com.xebia.functional.xef.llm.assistants.Tool
+import kotlinx.serialization.Serializable
+
+@Description("Natural numbers")
+enum class NaturalWithDescriptions {
+  @Description("If the number is positive.") POSITIVE,
+  @Description("If the number is negative.") NEGATIVE
+}
+
+@Serializable
+data class SumInputWithDescription(
+  @Description("Left operand") val left: Int,
+  @Description("Right operand") val right: Int,
+  val natural: NaturalWithDescriptions
+)
+
+class SumToolWithDescription : Tool<SumInputWithDescription, Int> {
+  override suspend fun invoke(input: SumInputWithDescription): Int {
+    return input.left + input.right
+  }
+}
+
+suspend fun main() {
+  val toolConfig = Tool.toolOf(SumToolWithDescription()).functionObject
+  println(toolConfig.parameters)
+}
+
+private suspend fun runAssistantAndDisplayResults(thread: AssistantThread, assistant: Assistant) {
+  thread.run(assistant).collect(RunDelta::printEvent)
+}
