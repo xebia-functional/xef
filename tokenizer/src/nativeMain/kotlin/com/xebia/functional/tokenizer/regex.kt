1110c85e45373c67188e176911400b8b43af6388
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-05-30 17:24:02 +0200

    fix: alpha numerics on rest of places

--- tokenizer/src/nativeMain/kotlin/com/xebia/functional/tokenizer/regex.kt
+++ tokenizer/src/nativeMain/kotlin/com/xebia/functional/tokenizer/regex.kt
@@ -67,8 +67,8 @@ actual val o200k_base_regex: Regex
   get() =
     Regex(
       listOf(
-          """[^\r\n\$alphaChars]?[$upperCaseAndOtherLetters]*[$lowerCaseAndOtherLetters]+('s|'t|'re|'ve|'m|'ll|'d)?""",
-          """[^\r\n\$alphaChars]?[$upperCaseAndOtherLetters]+[$lowerCaseAndOtherLetters]*('s|'t|'re|'ve|'m|'ll|'d)?""",
+          """[^\r\n\$alphaNumerics]?[$upperCaseAndOtherLetters]*[$lowerCaseAndOtherLetters]+('s|'t|'re|'ve|'m|'ll|'d)?""",
+          """[^\r\n\$alphaNumerics]?[$upperCaseAndOtherLetters]+[$lowerCaseAndOtherLetters]*('s|'t|'re|'ve|'m|'ll|'d)?""",
           """[$decimalNumbers]{1,3}""",
           """ ?[^\s\$alphaNumerics]+[\r\n/]*""",
           """\s*[\r\n]+""",
