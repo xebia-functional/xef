f7eccf1d0c16d955d0b1011552758b49e6b1caf0
Raúl Raja Martínez <raulraja@gmail.com>
2024-06-05 17:05:19 +0200

    Show request body on serialization error (#753)

--- openai-client/client/src/commonMain/kotlin/com/xebia/functional/openai/errors/ResponseSerializerErrors.kt
+++ openai-client/client/src/commonMain/kotlin/com/xebia/functional/openai/errors/ResponseSerializerErrors.kt
@@ -2,10 +2,32 @@ package com.xebia.functional.openai.errors
 
 import io.ktor.client.call.*
 import io.ktor.client.statement.*
+import io.ktor.http.content.*
 
 suspend inline fun <reified A> HttpResponse.serializeOrThrowWithResponseInfo(): A =
   try {
     this.body() ?: throw ResponseSerializerError("Response body is null")
   } catch (e: Exception) {
-    throw ResponseSerializerError("Failed to deserialize response body:\n${bodyAsText()}", e)
+    val requestBody =
+      when (val content = this.request.content) {
+        is OutgoingContent.ByteArrayContent -> content.bytes().decodeToString()
+        is OutgoingContent.NoContent -> "NoContent"
+        is OutgoingContent.ProtocolUpgrade -> "ProtocolUpgrade"
+        is OutgoingContent.ReadChannelContent -> "ReadChannelContent"
+        is OutgoingContent.WriteChannelContent -> "WriteChannelContent"
+        else -> "UnknownContent"
+      }
+    throw ResponseSerializerError(
+      """
+      |Failed to serialize response body to ${A::class.simpleName}
+      |Request URL: ${this.request.url}
+      |Request Method: ${this.request.method}
+      |Request Body: $requestBody
+      |Response Status: ${this.status}
+      |Response Headers: ${this.headers}
+      |Response Body: ${this.bodyAsText()}
+    """
+        .trimMargin(),
+      e
+    )
   }
